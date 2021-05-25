b3e22a14d5198fa3f783f51d9bc75b96b390f2b9
Chisanan232 <chi10211201@cycu.org.tw>
2021-05-25 23:07:49 +0800

    Modify import-path of module and class.

--- pyocean/api/mode.py
+++ pyocean/api/mode.py
@@ -7,6 +7,7 @@ _Package: str = "pyocean"
 # _Concurrent_Module: str = "..concurrent.features"
 _Parallel_Module: str = ".parallel.features"
 _Concurrent_Module: str = ".concurrent.features"
+_Coroutine_Module: str = ".coroutine.features"
 
 
 
@@ -14,6 +15,6 @@ class RunningMode(Enum):
 
     MultiProcessing: Dict[str, str] = {"module": _Parallel_Module, "class": "MultiProcessing"}
     MultiThreading: Dict[str, str] = {"module": _Concurrent_Module, "class": "MultiThreading"}
-    Coroutine: Dict[str, str] = {"module": _Concurrent_Module, "class": "Coroutine"}
-    Asynchronous: Dict[str, str] = {"module": _Concurrent_Module, "class": "Asynchronous"}
+    MultiGreenlet: Dict[str, str] = {"module": _Coroutine_Module, "class": "GeventAPI"}
+    Asynchronous: Dict[str, str] = {"module": _Coroutine_Module, "class": "AsynchronousAPI"}
 
