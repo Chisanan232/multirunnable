3053003b23011d37f0d72e357947fdaf1bb76002
Chisanan232 <chi10211201@cycu.org.tw>
2021-05-25 08:08:37 +0800

    Remove useless class 'CoroutineFactory'.

--- pyocean/concurrent/factory.py
+++ pyocean/concurrent/factory.py
@@ -1,6 +1,6 @@
 from pyocean.framework.factory import RunningFactory, RunningTask, RunnableStrategy
 from pyocean.persistence.interface import OceanPersistence
-from pyocean.concurrent.strategy import MultiThreadingStrategy, CoroutineStrategy
+from pyocean.concurrent.strategy import MultiThreadingStrategy
 
 from abc import ABC
 
@@ -12,15 +12,6 @@ class MultiThreadsFactory(RunningFactory, ABC):
         return MultiThreadingStrategy(threads_num=self._process_num,
                                       db_connection_pool_size=self._db_connection_num,
                                       persistence_strategy=persistence_strategy)
-
-
-
-class CoroutineFactory(RunningFactory, ABC):
-
-    def running_strategy(self, persistence_strategy: OceanPersistence) -> RunnableStrategy:
-        return CoroutineStrategy(threads_num=self._process_num,
-                                 db_connection_pool_size=self._db_connection_num,
-                                 persistence_strategy=persistence_strategy)
 
 
 
