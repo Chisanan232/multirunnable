78acbcfc611d2734a1ec7566e316c66c3c9d25d3
Chisanan232 <chi10211201@cycu.org.tw>
2021-05-25 12:24:16 +0800

    Add asyncronous strategy data type annotation.

--- pyocean/api/types.py
+++ pyocean/api/types.py
@@ -1,3 +1,46 @@
+"""
+Annotate all relative object (Queue, Lock, RLock, Semaphore, Bounded Semaphore, Event, Condition) of
+'running-strategy' --- multi-threading, multiprocessing, multi-greenlet and asynchronous.
+
+multi-threading:
+  No Queue object.
+    1. Lock
+    2. RLock
+    3. Semaphore
+    4. Bounded Semaphore
+    5. Event
+    6. Condition
+
+multiprocessing:
+    1. Queue
+    2. Lock
+    3. RLock
+    4. Semaphore
+    5. Bounded Semaphore
+    6. Event
+    7. Condition
+
+multi-greenlet:
+  No Lock object.
+    1. Queue
+    2. RLock
+    3. Semaphore
+    4. Bounded Semaphore
+    5. Event
+    6. Condition
+
+asynchronous:
+  No RLock object.
+    1. Queue
+    2. Lock
+    3. Semaphore
+    4. Bounded Semaphore
+    5. Event
+    6. Condition
+
+"""
+
+from queue import Queue
 from threading import (Lock as ThreadingLock,
                        RLock as ThreadingRLock,
                        Semaphore as ThreadingSemaphore,
@@ -14,14 +57,19 @@ from multiprocessing import (Queue as MultiProcessingQueue,
 from gevent.queue import Queue as GeventQueue
 from gevent.lock import RLock as GeventRLock, Semaphore as GeventSemaphore, BoundedSemaphore as GeventBoundedSemaphore
 from gevent.event import Event as GeventEvent
-from queue import Queue
+from asyncio.queues import Queue as AsyncIOQueue
+from asyncio.locks import (Lock as AsyncIOLock,
+                           Semaphore as AsyncIOSemaphore,
+                           BoundedSemaphore as AsyncIOBoundedSemaphore,
+                           Event as AsyncIOEvent,
+                           Condition as AsyncIOCondition)
 from typing import Union
 
 
-OceanQueue = Union[Queue, MultiProcessingQueue, GeventQueue]
-OceanLock = Union[ThreadingLock, MultiProcessingLock]
+OceanQueue = Union[Queue, MultiProcessingQueue, GeventQueue, AsyncIOQueue]
+OceanLock = Union[ThreadingLock, MultiProcessingLock, AsyncIOLock]
 OceanRLock = Union[ThreadingRLock, MultiProcessingRLock, GeventRLock]
-OceanSemaphore = Union[ThreadingSemaphore, MultiProcessingSemaphore, GeventSemaphore]
-OceanBoundedSemaphore = Union[ThreadingBoundedSemaphore, MultiProcessingBoundedSemaphore, GeventBoundedSemaphore]
-OceanEvent = Union[ThreadingEvent, MultiProcessingEvent, GeventEvent]
-OceanCondition = Union[ThreadingCondition, MultiProcessingCondition]
+OceanSemaphore = Union[ThreadingSemaphore, MultiProcessingSemaphore, GeventSemaphore, AsyncIOSemaphore]
+OceanBoundedSemaphore = Union[ThreadingBoundedSemaphore, MultiProcessingBoundedSemaphore, GeventBoundedSemaphore, AsyncIOBoundedSemaphore]
+OceanEvent = Union[ThreadingEvent, MultiProcessingEvent, GeventEvent, AsyncIOEvent]
+OceanCondition = Union[ThreadingCondition, MultiProcessingCondition, AsyncIOCondition]
