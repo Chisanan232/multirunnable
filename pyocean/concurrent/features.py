a1a8423829f498632ec363bda5cb81c827e8e3c3
Chisanan232 <chi10211201@cycu.org.tw>
2021-05-25 23:07:03 +0800

    Remove useless class.

--- pyocean/concurrent/features.py
+++ pyocean/concurrent/features.py
@@ -1,12 +1,13 @@
-from pyocean.framework.features import BaseAPI, BaseQueueType, FeatureUtils
-
-from deprecated.sphinx import deprecated
+from pyocean.framework.features import BaseAPI, BaseQueueType
 
 
 
 class MultiThreadingQueueType(BaseQueueType):
 
-    from queue import Queue as _Queue, SimpleQueue as _SimpleQueue, LifoQueue as _LifoQueue, PriorityQueue as _PriorityQueue
+    from queue import (Queue as _Queue, 
+                       SimpleQueue as _SimpleQueue, 
+                       LifoQueue as _LifoQueue, 
+                       PriorityQueue as _PriorityQueue)
 
     Queue = _Queue()
     SimpleQueue = _SimpleQueue()
@@ -49,119 +50,3 @@ class MultiThreading(BaseAPI):
 
     def queue(self, qtype: MultiThreadingQueueType):
         return qtype.value
-
-
-
-@deprecated(version="0.8", reason="Move the class into module 'coroutine'")
-class CoroutineQueueType(BaseQueueType):
-
-    from gevent.queue import Queue, SimpleQueue, JoinableQueue, PriorityQueue, LifoQueue
-
-    # Queue = Queue
-    # SimpleQueue = SimpleQueue
-    # JoinableQueue = JoinableQueue
-    # PriorityQueue = PriorityQueue
-    # LifoQueue = LifoQueue
-
-
-@deprecated(version="0.8", reason="Move the class into module 'coroutine'")
-class Coroutine(BaseAPI):
-
-    def lock(self):
-        from gevent.threading import Lock
-        return Lock()
-
-
-    def rlock(self):
-        from gevent.lock import RLock
-        return RLock()
-
-
-    def event(self, **kwargs):
-        from gevent.event import Event
-        return Event()
-
-
-    def condition(self, **kwargs):
-        raise Exception("Coroutine running strategy doesn't have method 'condition'. Please change to use 'event'.")
-
-
-    def semaphore(self, value: int):
-        from gevent.lock import Semaphore
-        return Semaphore(value=value)
-
-
-    def bounded_semaphore(self, value: int):
-        from gevent.lock import BoundedSemaphore
-        return BoundedSemaphore(value=value)
-
-
-    def queue(self, qtype: CoroutineQueueType):
-        return qtype.value()
-
-
-
-@deprecated(version="0.8", reason="Move the method into module 'coroutine'")
-class AsynchronousQueueType(BaseQueueType):
-
-    from asyncio.queues import Queue, PriorityQueue, LifoQueue
-
-    # Queue = Queue
-    # PriorityQueue = PriorityQueue
-    # LifoQueue = LifoQueue
-
-
-
-@deprecated(version="0.8", reason="Move the class into module 'coroutine'")
-class Asynchronous(BaseAPI):
-
-    def lock(self):
-        from asyncio.locks import Lock
-        return Lock()
-
-
-    def event(self, **kwargs):
-        from asyncio import Event
-        __loop = FeatureUtils.chk_obj(param="loop", **kwargs)
-        # __loop = self.__chk_loop_obj(**kwargs)
-        return Event(loop=__loop)
-
-
-    def condition(self, **kwargs):
-        from asyncio import Condition
-        __lock = FeatureUtils.chk_obj(param="lock", **kwargs)
-        __loop = FeatureUtils.chk_obj(param="loop", **kwargs)
-        # __lock = self.__chk_lock_obj(**kwargs)
-        # __loop = self.__chk_loop_obj(**kwargs)
-        return Condition(lock=__lock, loop=__loop)
-
-
-    @deprecated(version="0.6", reason="Move the method into class 'FeatureUtils'")
-    def __chk_lock_obj(self, **kwargs):
-        __lock = kwargs.get("lock")
-        if __lock is None:
-            raise Exception("")
-        return __lock
-
-
-    @deprecated(version="0.6", reason="Move the method into class 'FeatureUtils'")
-    def __chk_loop_obj(self, **kwargs):
-        __loop = kwargs.get("loop")
-        if __loop is None:
-            raise Exception("")
-        return __loop
-
-
-    def semaphore(self, value: int):
-        from asyncio.locks import Semaphore
-        return Semaphore(value=value)
-
-
-    def bounded_semaphore(self, value: int):
-        from asyncio.locks import BoundedSemaphore
-        return BoundedSemaphore(value=value)
-
-
-    def queue(self, qtype: AsynchronousQueueType):
-        return qtype.value()
-
