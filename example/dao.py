a593e6b6a351d18c29fe64fbd5c427eefce12a0f
Chisanan232 <chi10211201@cycu.org.tw>
2021-05-29 22:32:33 +0800

    Add new DAO class about handling data asynchronously.

--- example/dao.py
+++ example/dao.py
@@ -1,4 +1,4 @@
-from pyocean.framework import RunnableBuilder
+from pyocean.framework import MultiRunnableOperator, AsyncRunnableOperator
 from pyocean.persistence import OceanPersistence
 from pyocean.persistence.database import BaseDao, BaseConnection, SingleConnection, MultiConnections
 from pyocean.logger import OceanLogger
@@ -17,7 +17,7 @@ class TestDao(BaseDao):
         self._logger = logger
 
 
-    def run(self) -> object:
+    def run(self, *args, **kwargs) -> object:
         """
         Note (?) need to consider:
             How to abstractilize the logic or how to be more clear ?
@@ -27,12 +27,12 @@ class TestDao(BaseDao):
         if isinstance(self._Connection_Strategy, MultiConnections):
             # self._logger.debug("Running all things with Bounded Semaphore.")
             # data = RunnableBuilder.run_with_semaphore(function=self.sql_process_new)
-            data = RunnableBuilder.run_with_bounded_semaphore(function=self.sql_process_new)
+            data = MultiRunnableOperator.run_with_bounded_semaphore(function=self.sql_process_new)
             print("[DEBUG] at fun 'run': ", data)
             return data
         elif isinstance(self._Connection_Strategy, SingleConnection):
             # self._logger.debug("Running all things with Lock.")
-            return RunnableBuilder.run_with_lock(function=self.sql_process_new)
+            return MultiRunnableOperator.run_with_lock(function=self.sql_process_new)
         else:
             pass
 
@@ -68,3 +68,65 @@ class TestDao(BaseDao):
 
     def executing_result(self, running_state, data, exception_info) -> Dict[str, object]:
         return {"pid": os.getpid(), "state": running_state, "data": data, "exception_info": exception_info}
+
+
+
+class AsyncTestDao(BaseDao):
+
+    def __init__(self, connection_strategy: OceanPersistence, logger: OceanLogger):
+        super().__init__(connection_strategy=connection_strategy)
+        self._logger = logger
+
+
+    def run(self, *args, **kwargs) -> object:
+        """
+        Note (?) need to consider:
+            How to abstractilize the logic or how to be more clear ?
+        :return:
+        """
+        # Running directly
+        if isinstance(self._Connection_Strategy, MultiConnections):
+            # self._logger.debug("Running all things with Bounded Semaphore.")
+            # data = RunnableBuilder.run_with_semaphore(function=self.sql_process_new)
+            data = AsyncRunnableOperator.run_with_bounded_semaphore(function=self.sql_process_new)
+            print("[DEBUG] at fun 'run': ", data)
+            return data
+        elif isinstance(self._Connection_Strategy, SingleConnection):
+            # self._logger.debug("Running all things with Lock.")
+            return AsyncRunnableOperator.run_with_lock(function=self.sql_process_new)
+        else:
+            pass
+
+
+    async def sql_process_new(self):
+        sql_tasks = self.get_all_sql_tasks()
+        # self._logger.debug(f"SQL tasks: {sql_tasks}")
+        sql_query = await sql_tasks.get()
+        # self._logger.debug(f"SQL tasks query: {sql_query}")
+        data = self.running_sql_query_process(sql_query=sql_query)
+        # self._logger.debug(f"at fun 'sql_process_new': {data}")
+        return data
+
+
+    def execute_sql(self, cursor: MySQLCursor, query: str) -> object:
+        cursor.execute(query)
+        return cursor
+
+
+    def fetch_all(self, cursor: MySQLCursor) -> Union[object, None]:
+        return cursor.fetchall()
+
+
+    def error_handling(self, error: Error) -> Union[object, None]:
+        # Print error message
+        self._logger.error(f"Got an error! ")
+        self._logger.error(f"error: {error}")
+        self._logger.error(f"e.sqlstate: {error.sqlstate}")
+        self._logger.error(f"e.errno: {error.errno}")
+        self._logger.error(f"e.msg: {error.msg}")
+        return None
+
+
+    def executing_result(self, running_state, data, exception_info) -> Dict[str, object]:
+        return {"pid": os.getpid(), "state": running_state, "data": data, "exception_info": exception_info}
+
