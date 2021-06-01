64341536994fe2e66bf981ca57a354234401eef6
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-01 23:04:18 +0800

    Rename method 'running_director' to 'running_procedure'.

--- pyocean/framework/tasks.py
+++ pyocean/framework/tasks.py
@@ -1,6 +1,6 @@
-from pyocean.framework.factory import RunnableTaskFactory, SimpleTaskFactory, PersistenceTaskFactory
-from pyocean.framework.strategy import RunnableStrategy
 from pyocean.framework.operator import BaseRunnableProcedure
+from pyocean.framework.strategy import RunnableStrategy
+from pyocean.framework.factory import RunnableTaskFactory, SimpleTaskFactory, PersistenceTaskFactory
 from pyocean.persistence.interface import OceanPersistence
 
 from abc import ABC, ABCMeta, abstractmethod
@@ -28,7 +28,7 @@ class RunnableTask(metaclass=ABCMeta):
 
 
     @abstractmethod
-    def running_director(self, strategy: RunnableStrategy) -> BaseRunnableProcedure:
+    def running_procedure(self, strategy: RunnableStrategy) -> BaseRunnableProcedure:
         """
         Description:
             Return running builder object.
@@ -49,7 +49,7 @@ class AbstractedSimpleRunnableTask(RunnableTask, ABC):
         # Initial the running program strategy
         _running_strategy = self.running_strategy()
         # Initial the running procedure builder
-        _running_builder = self.running_director(strategy=_running_strategy)
+        _running_builder = self.running_procedure(strategy=_running_strategy)
         return _running_builder
 
 
@@ -86,7 +86,7 @@ class AbstractedPersistenceRunnableTask(RunnableTask, ABC):
         # Initial the running program strategy
         _running_strategy = self.running_strategy(persistence_strategy=_persistence_strategy)
         # Initial the running procedure builder
-        _running_builder = self.running_director(strategy=_running_strategy)
+        _running_builder = self.running_procedure(strategy=_running_strategy)
         return _running_builder
 
 
@@ -113,9 +113,9 @@ class AbstractedPersistenceRunnableTask(RunnableTask, ABC):
 
 class SimpleRunnableTask(AbstractedSimpleRunnableTask):
 
-    def running_director(self, strategy: RunnableStrategy) -> BaseRunnableProcedure:
-        _running_builder = self._factory.running_builder(running_strategy=strategy)
-        return _running_builder
+    def running_procedure(self, strategy: RunnableStrategy) -> BaseRunnableProcedure:
+        _running_procedure = self._factory.running_procedure(running_strategy=strategy)
+        return _running_procedure
 
 
     def running_strategy(self) -> RunnableStrategy:
@@ -126,9 +126,9 @@ class SimpleRunnableTask(AbstractedSimpleRunnableTask):
 
 class PersistenceRunnableTask(AbstractedPersistenceRunnableTask):
 
-    def running_director(self, strategy: RunnableStrategy) -> BaseRunnableProcedure:
-        _running_builder = self._factory.running_builder(running_strategy=strategy)
-        return _running_builder
+    def running_procedure(self, strategy: RunnableStrategy) -> BaseRunnableProcedure:
+        _running_procedure = self._factory.running_procedure(running_strategy=strategy)
+        return _running_procedure
 
 
     def running_strategy(self, persistence_strategy: OceanPersistence) -> RunnableStrategy:
@@ -140,60 +140,3 @@ class PersistenceRunnableTask(AbstractedPersistenceRunnableTask):
         _persistence_strategy = self._factory.persistence_strategy()
         return _persistence_strategy
 
-
-
-class RunningTask:
-
-    def __init__(self, process_number: int, db_connection_number: int, factory: RunnableTaskFactory):
-        self._process_num: int = process_number
-        self._db_connection_number: int = db_connection_number
-        self._factory: RunnableTaskFactory = factory
-        self._running_builder: BaseRunnableProcedure = None
-        self._running_strategy: RunnableStrategy = None
-        self._persistence_strategy: OceanPersistence = None
-
-
-    def running_builder(self) -> BaseRunnableProcedure:
-        """
-        Description:
-            Return running builder object.
-        :return:
-        """
-        return self._running_builder
-
-
-    def running_strategy(self) -> RunnableStrategy:
-        """
-        Description:
-            Return running strategy object.
-        :return:
-        """
-        return self._running_strategy
-
-
-    def persistence(self) -> OceanPersistence:
-        """
-        Description:
-            Return persistence strategy object.
-        :return:
-        """
-        return self._persistence_strategy
-
-
-    def generate(self) -> BaseRunnableProcedure:
-        """
-        Description:
-            generate a running task.
-        Note:
-            This method unifies the all of object which be needed to initialize before developer to use. Obviously, it be
-            implement via Facade Pattern. User just only need to focus the configurations (like how many number of
-            process or thread to use, etc.) and implementation details (like persistence strategy implement of MySQL).
-        :return:
-        """
-        # Initial the database connection building strategy
-        self._persistence_strategy = self._factory.persistence_strategy()
-        # Initial the running program strategy
-        self._running_strategy = self._factory.running_strategy(persistence_strategy=self._persistence_strategy)
-        # Initial the running procedure builder
-        self._running_builder = self._factory.running_builder(running_strategy=self._running_strategy)
-        return self._running_builder
