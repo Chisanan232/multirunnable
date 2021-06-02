69da2ccdf053b0a951de0585fc81f2453ebd6eae
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-02 21:49:30 +0800

    Adjust the import module.

--- pyocean/__init__.py
+++ pyocean/__init__.py
@@ -11,7 +11,11 @@ import sys
 __package_pyocean_path = str(pathlib.Path(__file__).parent.absolute())
 sys.path.append(__package_pyocean_path)
 
-
 # # Configure logging setting
 __user = getpass.getuser()
 PYOCEAN_LOGGER = getLogger(__user)
+
+from pyocean.parallel import ParallelProcedure, ParallelStrategy, ParallelSimpleFactory, ParallelPersistenceFactory
+from pyocean.concurrent import ConcurrentProcedure, MultiThreadingStrategy, MultiThreadsSimpleFactory, MultiThreadsPersistenceFactory
+from pyocean.coroutine import (GeventProcedure, GeventStrategy, GeventSimpleFactory, GeventPersistenceFactory,
+                               AsynchronousProcedure, AsynchronousStrategy, AsynchronousSimpleFactory, AsynchronousPersistenceFactory)
