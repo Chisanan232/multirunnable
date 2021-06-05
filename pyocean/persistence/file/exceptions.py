90c60d792d029f8373d02c62087e7bb50002d692
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-06 00:08:50 +0800

    Add a new exception.

--- pyocean/persistence/file/exceptions.py
+++ pyocean/persistence/file/exceptions.py
@@ -18,3 +18,8 @@ class NotSupportHandlingFileType(Exception):
     def __str__(self):
         return "It doesn't support to handle the file type currently. Please use JSON, CSV or Excel type file."
 
+
+class DataRowFormatIsInvalidError(RuntimeError):
+
+    def __str__(self):
+        return "Data row format is invalid. Please check the final step which you got  the result data from process."
