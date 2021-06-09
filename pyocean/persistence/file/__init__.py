ff074022d2e7b3ea801ad01eac8dd6892121c4e2
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-09 22:59:16 +0800

    Add new path which import module from persistence-file.

--- pyocean/persistence/file/__init__.py
+++ pyocean/persistence/file/__init__.py
@@ -1,2 +1,9 @@
 from pyocean.persistence.file.saver import SingleFileSaver, MultiFileSaver
-from pyocean.persistence.file.formatter import JSONFormatter, CSVFormatter, ExcelFormatter
+from pyocean.persistence.file.formatter import (JsonFileFormatter,
+                                                CsvFileFormatter,
+                                                ExcelFileFormatter,
+                                                JsonDataString,
+                                                CsvDataString)
+from pyocean.persistence.file.compress import ZipArchiver
+from pyocean.persistence.file.access_object import SingleFao, MultiFao
+from pyocean.persistence.file.configuration import FileConfig, ArchiverConfig, DefaultConfig
