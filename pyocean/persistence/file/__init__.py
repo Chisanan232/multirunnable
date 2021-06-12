44c045b7ea66751bd7a678a5522ae432485db837
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-12 12:56:16 +0800

    Update persistence-file __init__.

--- pyocean/persistence/file/__init__.py
+++ pyocean/persistence/file/__init__.py
@@ -1,9 +1,11 @@
-from pyocean.persistence.file.saver import SingleFileSaver, MultiFileSaver
-from pyocean.persistence.file.formatter import (JsonFileFormatter,
-                                                CsvFileFormatter,
-                                                ExcelFileFormatter,
-                                                JsonDataString,
-                                                CsvDataString)
-from pyocean.persistence.file.compress import ZipArchiver
-from pyocean.persistence.file.access_object import SingleFao, MultiFao
 from pyocean.persistence.file.configuration import FileConfig, ArchiverConfig, DefaultConfig
+from pyocean.persistence.file.saver import SingleFileSaver, ArchiverSaver
+from pyocean.persistence.file.file import (
+    JsonFileFormatter,
+    CsvFileFormatter,
+    XlsxFileFormatter,
+    JsonDataString,
+    CsvDataString
+)
+from pyocean.persistence.file.compress import ZipArchiver
+from pyocean.persistence.file.access_object import SimpleFileFao, SimpleArchiverFao
