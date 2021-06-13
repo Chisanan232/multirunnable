922b266ce2ef20f28fa4a108681215cffd7b23e4
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-13 15:24:46 +0800

    1. Add initailzie process about setting configuration value. 2. Modify the configuration path (file and archiver). 3. Configure archiver path.

--- pyocean/persistence/file/strategy.py
+++ pyocean/persistence/file/strategy.py
@@ -26,14 +26,20 @@ class BaseFaoStrategy(metaclass=ABCMeta):
     Above strategies, compress process isn't necessary.
     """
 
-    def __init__(self, config=None):
-        if config is None:
+    def __init__(self, file_config: Union[FileConfig, str] = None):
+        if file_config is None:
             self._file_name = FileConfig.file_name
             self._file_type = FileConfig.file_type
             self._dir = FileConfig.saving_directory
         else:
             # Do something configure setting (new feature)
-            pass
+            if type(file_config) is str:
+                __config = FileConfig(config_path=file_config)
+            else:
+                __config = file_config
+            self._file_name = __config.file_name
+            self._file_type = __config.file_type
+            self._dir = __config.saving_directory
 
 
     @abstractmethod
@@ -52,15 +58,22 @@ class BaseFaoWithFileStrategy(BaseFaoStrategy):
 
 class BaseFaoWithArchiverStrategy(BaseFaoStrategy):
 
-    def __init__(self, config=None):
-        super().__init__(config=config)
-        if config is None:
-            self.__archiver_type = ArchiverConfig.compress_type
-            self.__archiver_name = ArchiverConfig.compress_name
-            self.__archiver_path = ArchiverConfig.compress_path
+    def __init__(self, file_config: Union[FileConfig, str] = None,
+                 archiver_config: Union[ArchiverConfig, str] = None):
+        super().__init__(file_config=file_config)
+        if archiver_config is None:
+            self._archiver_type = ArchiverConfig.compress_type
+            self._archiver_name = ArchiverConfig.compress_name
+            self._archiver_path = ArchiverConfig.compress_path
         else:
             # Do something configure setting (new feature)
-            pass
+            if type(archiver_config) is str:
+                __config = ArchiverConfig(config_path=archiver_config)
+            else:
+                __config = archiver_config
+            self._archiver_type = __config.compress_type
+            self._archiver_name = __config.compress_name
+            self._archiver_path = __config.compress_path
 
 
     @abstractmethod
@@ -78,8 +91,9 @@ class OneThreadOneFile(BaseFaoWithFileStrategy):
 
     def file_path(self, **kwargs) -> List[str]:
         file_end = kwargs.get("file_end", "")
-        file_types = self._file_type.split(sep=",")
-        return [os.path.join(self._dir, f"{self._file_name}_{file_end}.{__file_type}") for __file_type in file_types]
+        # file_types = self._file_type.split(sep=",")
+        return [os.path.join(self._dir, f"{self._file_name}_{file_end}.{__file_type}")
+                for __file_type in self._file_type]
 
 
     def save_into_file(self, data: Union[List, Tuple],  formatter: BaseFileFormatter, **kwargs) -> None:
@@ -97,8 +111,8 @@ class OneThreadOneFile(BaseFaoWithFileStrategy):
 class AllThreadOneFile(BaseFaoWithFileStrategy):
 
     def file_path(self) -> List[str]:
-        file_types = self._file_type.split(sep=",")
-        return [os.path.join(self._dir, f"{self._file_name}.{__file_type}") for __file_type in file_types]
+        # file_types = self._file_type.split(sep=",")
+        return [os.path.join(self._dir, f"{self._file_name}.{__file_type}") for __file_type in self._file_type]
 
 
     def save_into_file(self, data: List,  formatter: BaseFileFormatter, **kwargs) -> None:
@@ -118,18 +132,19 @@ class OneThreadOneFileAllInArchiver(BaseFaoWithArchiverStrategy):
 
     def file_path(self, **kwargs) -> List[str]:
         file_end = kwargs.get("file_end", "")
-        file_types = self._file_type.split(sep=",")
-        return [f"{self._file_name}_{file_end}.{__file_type}" for __file_type in file_types]
+        # file_types = self._file_type.split(sep=",")
+        return [f"{self._file_name}_{file_end}.{__file_type}" for __file_type in self._file_type]
 
 
     def archiver_path(self) -> str:
-        archiver_file_name = f"{self.__archiver_name}.{self.__archiver_type}"
-        __path = os.path.join(self.__archiver_path, archiver_file_name)
+        archiver_file_name = f"{self._archiver_name}.{self._archiver_type}"
+        __path = os.path.join(self._archiver_path, archiver_file_name)
         return __path
 
 
     def save_and_compress(self, archiver: BaseArchiver, data: List, **kwargs):
         file_end = kwargs.get("file_end", "")
+        archiver.path = self.archiver_path()
         __archiver = ArchiverSaver(archiver=archiver)
         __archiver.save(file_path=self.file_path(file_end=file_end), data=data)
 
@@ -140,16 +155,17 @@ class AllThreadOneFileInArchiver(BaseFaoWithArchiverStrategy):
     __File_Strategy = AllThreadOneFile()
 
     def file_path(self) -> List[str]:
-        file_types = self._file_type.split(sep=",")
-        return [f"{self._file_name}.{__file_type}" for __file_type in file_types]
+        # file_types = self._file_type.split(sep=",")
+        return [f"{self._file_name}.{__file_type}" for __file_type in self._file_type]
 
 
     def archiver_path(self) -> str:
-        archiver_file_name = f"{self.__archiver_name}.{self.__archiver_type}"
-        __path = os.path.join(self.__archiver_path, archiver_file_name)
+        archiver_file_name = f"{self._archiver_name}.{self._archiver_type}"
+        __path = os.path.join(self._archiver_path, archiver_file_name)
         return __path
 
 
     def save_and_compress(self, archiver: BaseArchiver, data: List, **kwargs):
+        archiver.path = self.archiver_path()
         __archiver = ArchiverSaver(archiver=archiver)
         __archiver.save(file_path=self.file_path(), data=data)
