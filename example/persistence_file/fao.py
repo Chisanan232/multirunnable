76b56e724b77eefffefacb549bfb48fb06114f83
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-14 22:17:38 +0800

    Add file access object with new version.

--- example/persistence_file/fao.py
+++ example/persistence_file/fao.py
@@ -1,54 +1,39 @@
-from pyocean.persistence.file import SingleFileSaver, MultiFileSaver, JSONFormatter, CSVFormatter, ExcelFormatter
+from pyocean.persistence.file import SimpleFileFao, SimpleArchiverFao
 
-from typing import Iterable, List
-import logging
-
-
-
-class TestFao:
-
-    __JSON_File_Path = "<.json type path>"
-    __CSV_File_Path = "<.csv type path>"
-    __Excel_File_Path = "<.xlsx type path>"
-
-    @classmethod
-    def save_data_as_json(cls, data: Iterable):
-        __saver = SingleFileSaver(file_path=cls.__JSON_File_Path, file_format=JSONFormatter())
-        if isinstance(data, List):
-            __saver.save(data=data)
-        else:
-            logging.warning("The data structure is not perfectly mapping.")
-            __saver.save(data=data)
-
-
-    @classmethod
-    def save_data_as_csv(cls, data: Iterable):
-        __saver = SingleFileSaver(file_path=cls.__CSV_File_Path, file_format=CSVFormatter())
-        if isinstance(data, List):
-            __saver.save(data=data)
-        else:
-            logging.warning("The data structure is not perfectly mapping.")
-            __saver.save(data=data)
-
-
-    @classmethod
-    def save_data_as_excel(cls, data: Iterable):
-        __saver = SingleFileSaver(file_path=cls.__Excel_File_Path, file_format=ExcelFormatter())
-        if isinstance(data, List):
-            __saver.save(data=data)
-        else:
-            logging.warning("The data structure is not perfectly mapping.")
-            __saver.save(data=data)
+from typing import Iterable, List, Union, Tuple
 
 
 
-class TestAsyncFao:
+class ExampleFao:
 
-    @classmethod
-    def save_data_as_json(cls, data: Iterable):
-        __saver = SingleFileSaver(file_path="", file_format=JSONFormatter())
-        if isinstance(data, List):
-            __saver.save(data=data)
-        else:
-            logging.warning("The data structure is not perfectly mapping.")
-            __saver.save(data=data)
+    __Config_Path = "The .properties configuration file path."
+
+    def __init__(self):
+        self.__file_fao = SimpleFileFao(config=self.__Config_Path)
+        self.__archiver_fao = SimpleArchiverFao(config=self.__Config_Path)
+
+
+    def one_thread_one_file(self, data: List, file_end: str):
+        self.__file_fao.one_thread_one_file(
+            data=data,
+            file_end=file_end
+        )
+
+
+    def all_thread_one_file(self, data: List):
+        self.__file_fao.all_thread_one_file(
+            data=data
+        )
+
+
+    def one_thread_one_file_all_in_archiver(self, data: List, file_end: str):
+        self.__archiver_fao.one_thread_one_file_all_in_archiver(
+            archiver=self.__archiver,
+            data=data,
+            file_end=file_end
+        )
+
+
+    def all_thread_one_file_in_archiver(self, data: List):
+        self.__archiver_fao.all_thread_one_file_in_archiver(data=data)
+
