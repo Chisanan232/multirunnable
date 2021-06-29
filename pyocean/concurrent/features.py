fb9514a737cb1a88126a4823d49d10491cac4718
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-29 22:18:19 +0800

    Add PosixThreadLock, PosixThreadCommunication and BaseQueue implements.

--- pyocean/concurrent/features.py
+++ pyocean/concurrent/features.py
@@ -1,52 +1,91 @@
-from pyocean.framework.features import BaseAPI, BaseQueueType
+from pyocean.framework.features import PosixThreadLock, PosixThreadCommunication, BaseQueue, BaseAPI, BaseQueueType
 
+from threading import Lock, RLock, Event, Condition, Semaphore, BoundedSemaphore
+from queue import (
+    Queue as Thread_Queue,
+    SimpleQueue as Thread_SimpleQueue,
+    LifoQueue as Thread_LifoQueue,
+    PriorityQueue as Thread_PriorityQueue)
+from typing import Union
+
+
+
+ThreadQueueDataType = Union[Thread_Queue, Thread_SimpleQueue, Thread_LifoQueue, Thread_PriorityQueue]
 
 
 class MultiThreadingQueueType(BaseQueueType):
 
-    from queue import (Queue as _Queue, 
-                       SimpleQueue as _SimpleQueue, 
-                       LifoQueue as _LifoQueue, 
-                       PriorityQueue as _PriorityQueue)
+    Queue = Thread_Queue()
+    SimpleQueue = Thread_SimpleQueue()
+    LifoQueue = Thread_LifoQueue()
+    PriorityQueue = Thread_PriorityQueue()
 
-    Queue = _Queue()
-    SimpleQueue = _SimpleQueue()
-    LifoQueue = _LifoQueue()
-    PriorityQueue = _PriorityQueue()
+
+
+class ThreadQueue(BaseQueue):
+
+    def get_queue(self, qtype: MultiThreadingQueueType) -> ThreadQueueDataType:
+        return qtype.value
 
 
 
 class MultiThreading(BaseAPI):
 
     def lock(self):
-        from threading import Lock
         return Lock()
 
 
     def rlock(self):
-        from threading import RLock
         return RLock()
 
 
     def event(self, **kwargs):
-        from threading import Event
         return Event()
 
 
     def condition(self, **kwargs):
-        from threading import Condition
         return Condition()
 
 
     def semaphore(self, value: int):
-        from threading import Semaphore
         return Semaphore(value=value)
 
 
     def bounded_semaphore(self, value: int):
-        from threading import BoundedSemaphore
         return BoundedSemaphore(value=value)
 
 
     def queue(self, qtype: MultiThreadingQueueType):
         return qtype.value
+
+
+
+class ThreadLock(PosixThreadLock):
+
+    def get_lock(self) -> Lock:
+        return Lock()
+
+
+    def get_rlock(self) -> RLock:
+        return RLock()
+
+
+    def get_semaphore(self, value: int) -> Semaphore:
+        return Semaphore(value=value)
+
+
+    def get_bounded_semaphore(self, value: int) -> BoundedSemaphore:
+        return BoundedSemaphore(value=value)
+
+
+
+class ThreadCommunication(PosixThreadCommunication):
+
+    def get_event(self, *args, **kwargs) -> Event:
+        return Event()
+
+
+    def get_condition(self, *args, **kwargs) -> Condition:
+        __lock: Union[Lock, RLock, None] = kwargs.get("lock", None)
+        return Condition(lock=__lock)
+
