ecd0c44a8a9e5d5693d98d515b902938a1d17c77
Chisanan232 <chi10211201@cycu.org.tw>
2021-06-29 22:18:05 +0800

    Add PosixThreadLock, PosixThreadCommunication and BaseQueue implements.

--- pyocean/parallel/features.py
+++ pyocean/parallel/features.py
@@ -1,10 +1,17 @@
-from pyocean.framework.features import BaseAPI, BaseQueueType, FeatureUtils
+from pyocean.framework.features import PosixThreadLock, PosixThreadCommunication, BaseQueue, BaseAPI, BaseQueueType, FeatureUtils
 
 from multiprocessing import Lock, RLock, Event, Condition, Semaphore, BoundedSemaphore
-from multiprocessing import Queue as Process_Queue, SimpleQueue as Process_SimpleQueue, JoinableQueue as Process_JoinableQueue
+from multiprocessing import (
+    Queue as Process_Queue,
+    SimpleQueue as Process_SimpleQueue,
+    JoinableQueue as Process_JoinableQueue)
+from typing import Union
 
 from deprecated.sphinx import deprecated
 
+
+
+ProcessQueueDataType = Union[Process_Queue, Process_SimpleQueue, Process_JoinableQueue]
 
 
 class MultiProcessingQueueType(BaseQueueType):
@@ -12,6 +19,13 @@ class MultiProcessingQueueType(BaseQueueType):
     Queue = Process_Queue()
     SimpleQueue = Process_SimpleQueue()
     JoinableQueue = Process_JoinableQueue()
+
+
+
+class ProcessQueue(BaseQueue):
+
+    def get_queue(self, qtype: MultiProcessingQueueType) -> ProcessQueueDataType:
+        return qtype.value
 
 
 
@@ -26,9 +40,7 @@ class MultiProcessing(BaseAPI):
 
 
     def event(self, **kwargs):
-        __lock = FeatureUtils.chk_obj(param="lock", **kwargs)
-        # __lock = self.__chk_lock_obj(**kwargs)
-        return Event(lock=__lock)
+        return Event()
 
 
     def condition(self, **kwargs):
@@ -55,3 +67,35 @@ class MultiProcessing(BaseAPI):
 
     def queue(self, qtype: MultiProcessingQueueType):
         return qtype.value
+
+
+
+class ProcessLock(PosixThreadLock):
+
+    def get_lock(self) -> Lock:
+        return Lock()
+
+
+    def get_rlock(self) -> RLock:
+        return RLock()
+
+
+    def get_semaphore(self, value: int) -> Semaphore:
+        return Semaphore(value=value)
+
+
+    def get_bounded_semaphore(self, value: int) -> BoundedSemaphore:
+        return BoundedSemaphore(value=value)
+
+
+
+class ProcessCommunication(PosixThreadCommunication):
+
+    def get_event(self, *args, **kwargs) -> Event:
+        return Event()
+
+
+    def get_condition(self, *args, **kwargs) -> Condition:
+        __lock: Union[Lock, RLock, None] = kwargs.get("lock", None)
+        return Condition(lock=__lock)
+
