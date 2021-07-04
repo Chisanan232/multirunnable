c3af6bc4e65fac35f15ac2c58d8cf942058ac4a9
Chisanan232 <chi10211201@cycu.org.tw>
2021-07-04 14:29:24 +0800

    Remove the None case handling in init process.

--- pyocean/persistence/file/access_object.py
+++ pyocean/persistence/file/access_object.py
@@ -29,6 +29,7 @@ class BaseFao(OceanFao):
         except Exception as e:
             self.exception_handling(error=e)
             error = e
+            checksum = False
         else:
             checksum = True
         finally:
@@ -54,29 +55,25 @@ class SimpleFileFao(BaseFao):
 
     __ID_Checksum = None
 
-    def __init__(self, config: Union[FileConfig, str] = None):
-        if config is None:
-            # Get data from properties file
-            self._config = FileConfig()
-        else:
-            if type(config) is str:
-                # Check and initialize configuration by the file path.
-                is_file = os.path.isfile(path=config)
-                file_extension = config.split(sep=".")[-1]
-                is_properties_extension = re.search(r"properties", file_extension)
-                if is_file and is_properties_extension:
-                    self._config = config
-                else:
-                    raise Exception("It's not a valid file path.")
-            else:
-                # Check and integrate the config value to use.
-                if config.file_type:
-                    raise Exception("File type value is empty.")
-                if config.file_name:
-                    raise Exception("File name value is empty.")
-                if config.saving_directory:
-                    raise Exception("Directory path value is empty.")
+    def __init__(self, config: Union[FileConfig, str]):
+        if type(config) is str:
+            # Check and initialize configuration by the file path.
+            is_file = os.path.isfile(path=config)
+            file_extension = config.split(sep=".")[-1]
+            is_properties_extension = re.search(r"properties", file_extension)
+            if is_file and is_properties_extension:
                 self._config = config
+            else:
+                raise Exception("It's not a valid file path.")
+        else:
+            # Check and integrate the config value to use.
+            if config.file_type:
+                raise Exception("File type value is empty.")
+            if config.file_name:
+                raise Exception("File name value is empty.")
+            if config.saving_directory:
+                raise Exception("Directory path value is empty.")
+            self._config = config
 
 
     def one_thread_one_file(self, data: Union[List, Tuple], **kwargs):
@@ -122,41 +119,36 @@ class SimpleArchiverFao(BaseFao):
 
     __ID_Checksum = None
 
-    def __init__(self, config: Union[Tuple[BaseFileConfig], str] = None):
-        if config is None:
-            # Get data from properties file
-            self._file_config = FileConfig()
-            self._archiver_config = ArchiverConfig()
-        else:
-            if type(config) is str:
-                # Check and initialize configuration by the file path.
-                is_file = os.path.isfile(path=config)
-                file_extension = config.split(sep=".")[-1]
-                is_properties_extension = re.search(r"properties", file_extension)
-                if is_file and is_properties_extension:
-                    # self._config = config
-                    self._file_config = FileConfig(config_path=config)
-                    self._archiver_config = ArchiverConfig(config_path=config)
-                else:
-                    raise Exception("It's not a valid file path.")
+    def __init__(self, config: Union[Tuple[BaseFileConfig], str]):
+        if type(config) is str:
+            # Check and initialize configuration by the file path.
+            is_file = os.path.isfile(path=config)
+            file_extension = config.split(sep=".")[-1]
+            is_properties_extension = re.search(r"properties", file_extension)
+            if is_file and is_properties_extension:
+                # self._config = config
+                self._file_config = FileConfig(config_path=config)
+                self._archiver_config = ArchiverConfig(config_path=config)
             else:
-                __file_config = filter(lambda c: isinstance(c, FileConfig), config)
-                __archiver_config = filter(lambda c: isinstance(c, ArchiverConfig), config)
-                self._file_config: FileConfig = list(__file_config)[0]
-                self._archiver_config: ArchiverConfig = list(__archiver_config)[0]
-                # Check and integrate the config value to use.
-                if self._file_config.file_type:
-                    raise Exception("File type value is empty.")
-                if self._file_config.file_name:
-                    raise Exception("File name value is empty.")
-                if self._file_config.saving_directory:
-                    raise Exception("Directory path value is empty.")
-                if self._archiver_config.compress_type:
-                    raise Exception("Archiver type value is empty.")
-                if self._archiver_config.compress_name:
-                    raise Exception("Archiver name value is empty.")
-                if self._archiver_config.compress_path:
-                    raise Exception("Archiver path value is empty.")
+                raise Exception("It's not a valid file path.")
+        else:
+            __file_config = filter(lambda c: isinstance(c, FileConfig), config)
+            __archiver_config = filter(lambda c: isinstance(c, ArchiverConfig), config)
+            self._file_config: FileConfig = list(__file_config)[0]
+            self._archiver_config: ArchiverConfig = list(__archiver_config)[0]
+            # Check and integrate the config value to use.
+            if self._file_config.file_type:
+                raise Exception("File type value is empty.")
+            if self._file_config.file_name:
+                raise Exception("File name value is empty.")
+            if self._file_config.saving_directory:
+                raise Exception("Directory path value is empty.")
+            if self._archiver_config.compress_type:
+                raise Exception("Archiver type value is empty.")
+            if self._archiver_config.compress_name:
+                raise Exception("Archiver name value is empty.")
+            if self._archiver_config.compress_path:
+                raise Exception("Archiver path value is empty.")
 
 
     def one_thread_one_file_all_in_archiver(self, data: Union[List, Tuple], **kwargs):
