6e1e0194f2bc5ffec05d79531923324c5839f62f
Chisanan232 <chi10211201@cycu.org.tw>
2021-07-31 14:59:25 +0800

    1. Renaming the original abstracted OceanResult to BaseResult. 2. Let OceanResult be the implementation of BaseResult and also be a template object.

--- pyocean/framework/result.py
+++ pyocean/framework/result.py
@@ -13,7 +13,7 @@ class ResultState(Enum):
 
 
 
-class OceanResult(metaclass=ABCMeta):
+class BaseResult(metaclass=ABCMeta):
 
     @property
     @abstractmethod
@@ -44,3 +44,61 @@ class OceanResult(metaclass=ABCMeta):
     def exception(self) -> Exception:
         pass
 
+
+
+class OceanResult(BaseResult):
+
+    _PID: str = ""
+    _Worker_ID: str = ""
+    _Data: List[Any] = []
+    _State: ResultState = None
+    _Exception: Exception = None
+
+    @property
+    def pid(self) -> str:
+        return self._PID
+
+
+    @pid.setter
+    def pid(self, pid: str) -> None:
+        self._PID = pid
+
+
+    @property
+    def worker_id(self) -> str:
+        return self._Worker_ID
+
+
+    @worker_id.setter
+    def worker_id(self, worker_id) -> None:
+        self._Worker_ID = worker_id
+
+
+    @property
+    def data(self) -> List[Any]:
+        return self._Data
+
+
+    @data.setter
+    def data(self, data: List[Any]) -> None:
+        self._Data = data
+
+
+    @property
+    def state(self) -> ResultState:
+        return self._State
+
+
+    @state.setter
+    def state(self, state: ResultState) -> None:
+        self._State = state
+
+
+    @property
+    def exception(self) -> Exception:
+        return self._Exception
+
+
+    @exception.setter
+    def exception(self, exception: Exception) -> None:
+        self._Exception = exception
