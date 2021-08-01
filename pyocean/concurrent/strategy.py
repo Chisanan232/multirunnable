09382142cb49411de5d243bd541780b616716b29
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-01 12:18:59 +0800

    1. Modify the detail implementation of method 'build_workers'. 2. Add decorator for feature about returning result data and saving it into list. 3. Implement handling return value as ConcurrentResult.

--- pyocean/concurrent/strategy.py
+++ pyocean/concurrent/strategy.py
@@ -1,13 +1,18 @@
 from pyocean.framework.strategy import InitializeUtils, RunnableStrategy, Resultable
-from pyocean.framework.features import BaseQueueType
+from pyocean.framework.worker import BaseTask
+from pyocean.worker import OceanTask
+# from pyocean.framework.features import BaseQueueType
 from pyocean.api import FeatureMode
-from pyocean.types import OceanTasks
-from pyocean.concurrent.features import MultiThreadingQueueType
+from pyocean.api.mode import RunningMode
+# from pyocean.concurrent.features import MultiThreadingQueueType
+from pyocean.concurrent.result import ConcurrentResult
+from pyocean.exceptions import FunctionSignatureConflictError
 from pyocean.concurrent.exceptions import ThreadsListIsEmptyError
 
 from abc import abstractmethod, ABC
-from typing import List, Tuple, Dict, Iterable, Union, Callable, Any
+from typing import List, Dict, Union, Callable
 from threading import Thread
+from functools import wraps
 
 
 
@@ -18,7 +23,7 @@ class ConcurrentStrategy(RunnableStrategy, ABC):
     _Threads_Running_Result: Dict[str, Dict[str, Union[object, bool]]] = {}
     _Threading_Running_Result: List = []
 
-    def activate_workers(self, workers_list: List[OceanTasks]) -> None:
+    def activate_workers(self, workers_list: List[Thread]) -> None:
         # # Method 1.
         for worker in workers_list:
             self.activate_worker(worker=worker)
@@ -29,7 +34,7 @@ class ConcurrentStrategy(RunnableStrategy, ABC):
 
 
     @abstractmethod
-    def activate_worker(self, worker: OceanTasks) -> None:
+    def activate_worker(self, worker: Thread) -> None:
         """
         Description:
             Each one thread or process running task implementation.
@@ -52,38 +57,61 @@ class MultiThreadingStrategy(ConcurrentStrategy, Resultable):
             __init_utils.initialize_persistence(db_conn_instances_num=self.db_connection_number)
 
 
-    def build_workers(self, function: Callable, *args, **kwargs) -> List[Thread]:
+    def build_workers_og(self, function: Callable, *args, **kwargs) -> List[Thread]:
         self._Threads_List = [Thread(target=function, args=args, kwargs=kwargs) for _ in range(self.workers_number)]
         return self._Threads_List
 
 
-    def build_workers_test(self, function: Callable, args: Tuple = (), kwargs: Dict = {}) -> List[Thread]:
+    def build_workers(self, function: Callable, *args, **kwargs) -> List[Thread]:
+
+        def __general_task() -> OceanTask:
+            __otask = OceanTask(mode=RunningMode.Concurrent)
+            __otask.set_function(function=function)
+            __otask.set_func_args(args=args)
+            __otask.set_func_kwargs(kwargs=kwargs)
+            return __otask
+
         if args:
-            args = (function,) + args
+            __task = filter(lambda arg: isinstance(arg, BaseTask), args)
+            if __task is None:
+                __task = __general_task()
+            __args = (__task,)
+        else:
+            __args = ()
+
         if kwargs:
-            kwargs["function"] = function
-        self._Threads_List = [Thread(target=self.target_function, args=args, kwargs=kwargs) for _ in range(self.workers_number)]
+            __task = kwargs.get("task", None)
+            if __task is None:
+                __task = __general_task()
+            else:
+                if not isinstance(__task, BaseTask):
+                    raise FunctionSignatureConflictError
+            __kwargs = {"task": __task}
+        else:
+            __kwargs = {}
+
+        self._Threads_List = [Thread(target=self.target_task, args=__args, kwargs=__kwargs) for _ in range(self.workers_number)]
         return self._Threads_List
 
 
-    def record_result(function: Callable) -> Callable:
+    def save_return_value(function: Callable) -> Callable:
 
-        def decorator(self, *args, **kwargs) -> None:
-            print(f"record_result.args: {args}")
-            print(f"record_result.kwargs: {kwargs}")
-            value = function(self, *args, **kwargs)
-            self._Threading_Running_Result.append(value)
+        @wraps(function)
+        def save_value_fun(self, task: BaseTask) -> None:
+            self = self
+            value = task.function(*task.func_args, **task.func_kwargs)
+            __thread_result = {"result": value}
+            self._Threading_Running_Result.append(__thread_result)
 
-        return decorator
-
-
-    @record_result
-    def target_function(self, function: Callable, *args, **kwargs) -> Union[None, Any]:
-        value = function(*args, **kwargs)
-        return value
+        return save_value_fun
 
 
-    def activate_worker(self, worker: OceanTasks) -> None:
+    @save_return_value
+    def target_task(self, task: BaseTask) -> None:
+        task.function(self, *task.func_args, **task.func_kwargs)
+
+
+    def activate_worker(self, worker: Thread) -> None:
         worker.start()
 
 
@@ -95,5 +123,17 @@ class MultiThreadingStrategy(ConcurrentStrategy, Resultable):
             raise ThreadsListIsEmptyError
 
 
-    def get_result(self) -> Iterable[object]:
-        return self._Threading_Running_Result
+    def get_result(self) -> List[ConcurrentResult]:
+        __concurrent_result = self._result_handling()
+        return __concurrent_result
+
+
+    def _result_handling(self) -> List[ConcurrentResult]:
+        __concurrent_results = []
+        for __result in self._Threading_Running_Result:
+            __concurrent_result = ConcurrentResult()
+            __concurrent_result.data = __result.get("result")
+
+            __concurrent_results.append(__concurrent_result)
+
+        return __concurrent_results
