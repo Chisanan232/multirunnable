552d55ca0d852aa052a1be5a2c2cb30e1867cdc5
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-05 23:19:44 +0800

    1. Rename the root modules for strategy to be 'BaseRunnableStrategy'. /r/n 2. Add new abstracted class module 'RunnableStratgy' which implement common initialization detail. /r/n 3. Add asynchronous version of initialization detail implementation in asynchronous version of 'RunnableStrategy'. /r/n 4. Modify the description about methods.

--- pyocean/framework/strategy.py
+++ pyocean/framework/strategy.py
@@ -1,7 +1,9 @@
 from pyocean.types import OceanTasks
 from pyocean.framework.task import BaseQueueTask
+from pyocean.framework.features import BaseFeatureAdapterFactory
+from pyocean.framework.exceptions import FeatureFactoryCannotBeEmpty
 from pyocean.api.mode import FeatureMode
-from pyocean.api.features_adapter import Feature, QueueAdapter, LockAdapter, CommunicationAdapter
+from pyocean.api.tool import Feature
 from pyocean.api.manager import Globalize as RunningGlobalize
 from pyocean.persistence.interface import OceanPersistence
 
@@ -11,27 +13,32 @@ import logging
 
 
 
-class RunnableStrategy(metaclass=ABCMeta):
+class BaseRunnableStrategy(metaclass=ABCMeta):
 
     _Running_Feature_Mode: Optional[FeatureMode] = None
 
-    def __init__(self, workers_num: int, persistence_strategy: OceanPersistence = None, **kwargs):
-        self.__workers_num = workers_num
+    def __init__(self, workers_num: int,
+                 features_factory: BaseFeatureAdapterFactory = None,
+                 persistence_strategy: OceanPersistence = None, **kwargs):
+        self._workers_num = workers_num
+        self._features_factory = features_factory
         self._persistence_strategy = persistence_strategy
-        self.__db_conn_num = kwargs.get("db_connection_pool_size", None)
+        self._db_conn_num = kwargs.get("db_connection_pool_size", None)
 
 
     @property
+    @abstractmethod
     def workers_number(self) -> int:
         """
         Description:
             The number of threads or processes be create and activate to do something.
         :return:
         """
-        return self.__workers_num
+        pass
 
 
     @property
+    @abstractmethod
     def db_connection_number(self) -> int:
         """
         Description:
@@ -40,19 +47,7 @@ class RunnableStrategy(metaclass=ABCMeta):
             The number be suggested to be roughly equal to the CPUs amount of host which the program be run.
         :return:
         """
-        from multiprocessing import cpu_count
-
-        if self.__db_conn_num is None:
-            if self.__workers_num < cpu_count():
-                return self.__workers_num
-            else:
-                return cpu_count()
-        else:
-            if self.__db_conn_num > cpu_count():
-                logging.warning("Warning about suggestion is the best "
-                                "configuration of database connection instance "
-                                "should be less than CPU amounts.")
-            return self.__db_conn_num
+        pass
 
 
     def initialization(self, queue_tasks: Optional[List[BaseQueueTask]] = None,
@@ -67,80 +62,7 @@ class RunnableStrategy(metaclass=ABCMeta):
         :param kwargs:
         :return:
         """
-
-        # # Semaphore part (Limitation)
-        # # # # Think about how to design the initialization like lock, semaphore or queue
-        # # # # beside design, needs to more think about how to use it? how to extend it? how to maintain it?
-        # # Queue initialization
-        if queue_tasks is not None:
-            self._init_queue_process(tasks=queue_tasks)
-
-        # # Filter mechanism
-        __lock_features = filter(lambda __feature: __feature not in [Feature.Event, Feature.Condition], features)
-        __communication_features = filter(lambda __feature: __feature in [Feature.Event, Feature.Condition], features)
-
-        # # Lock initialization
-        if __lock_features:
-            self._init_lock_process(features=__lock_features)
-
-        # # Communication initialization
-        if __communication_features:
-            self._init_communication_process(features=__communication_features)
-
-
-    def _init_queue_process(self, tasks: List[BaseQueueTask]) -> None:
-        """
-        Initialize Queue object which be needed to handle in Queue-Task-List.
-        :param tasks:
-        :return:
-        """
-
-        __queue_adapter = QueueAdapter(mode=self._Running_Feature_Mode)
-        for task in tasks:
-            __queue = __queue_adapter.init_queue_with_values(qtype=task.queue_type, values=task.value)
-            RunningGlobalize.queue(name=task.name, queue=__queue)
-
-
-    def _init_lock_process(self, features: Iterable[Feature], **adapter_kwargs) -> None:
-        """
-        Initialize Lock object (Lock, RLock, Semaphore, Bounded Semaphore)
-        which be needed to handle in Feature-List.
-        :param features:
-        :return:
-        """
-
-        __lock_adapter = LockAdapter(mode=self._Running_Feature_Mode, **adapter_kwargs)
-        for feature in features:
-            if feature == Feature.Lock:
-                __lock = __lock_adapter.get_lock()
-                RunningGlobalize.lock(lock=__lock)
-            if feature == Feature.RLock:
-                __rlock = __lock_adapter.get_rlock()
-                RunningGlobalize.rlock(rlock=__rlock)
-            if feature == Feature.Semaphore:
-                __semaphore = __lock_adapter.get_semaphore(value=self.db_connection_number)
-                RunningGlobalize.semaphore(smp=__semaphore)
-            if feature == Feature.Bounded_Semaphore:
-                __bounded_semaphore = __lock_adapter.get_bounded_semaphore(value=self.db_connection_number)
-                RunningGlobalize.bounded_semaphore(bsmp=__bounded_semaphore)
-
-
-    def _init_communication_process(self, features: Iterable[Feature], **adapter_kwargs) -> None:
-        """
-        Initialize Lock object (Event, Condition) which be needed to
-        handle in Feature-List.
-        :param features:
-        :return:
-        """
-
-        __communication_adapter = CommunicationAdapter(mode=self._Running_Feature_Mode, **adapter_kwargs)
-        for feature in features:
-            if feature.Event:
-                __event = __communication_adapter.get_event()
-                RunningGlobalize.event(event=__event)
-            if feature.Condition:
-                __condition = __communication_adapter.get_event()
-                RunningGlobalize.condition(condition=__condition)
+        pass
 
 
     @abstractmethod
@@ -178,18 +100,206 @@ class RunnableStrategy(metaclass=ABCMeta):
 
 
 
+class RunnableStrategy(BaseRunnableStrategy, ABC):
+
+    @property
+    def workers_number(self) -> int:
+        """
+        Description:
+            The number of threads or processes be create and activate to do something.
+        :return:
+        """
+        return self._workers_num
+
+
+    @property
+    def db_connection_number(self) -> int:
+        """
+        Description:
+            The number of the connection instances which target to do something operators with database.
+        Note:
+            The number be suggested to be roughly equal to the CPUs amount of host which the program be run.
+        :return:
+        """
+        from multiprocessing import cpu_count
+
+        if self._db_conn_num is None:
+            if self._workers_num < cpu_count():
+                return self._workers_num
+            else:
+                return cpu_count()
+        else:
+            if self._db_conn_num > cpu_count():
+                logging.warning("Warning about suggestion is the best "
+                                "configuration of database connection instance "
+                                "should be less than CPU amounts.")
+            return self._db_conn_num
+
+
+    def initialization(self, queue_tasks: Optional[List[BaseQueueTask]] = None,
+                       features: Optional[List[Feature]] = None, *args, **kwargs) -> None:
+        """
+        Description:
+            Initialize something configurations or something which be needed to be already before run multiple
+            threads or processes.
+        :param queue_tasks:
+        :param features:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        # # # # Think about how to design the initialization like lock, semaphore or queue
+        # # # # beside design, needs to more think about how to use it? how to extend it? how to maintain it?
+        # # Queue initialization
+        if queue_tasks is not None:
+            self._features_factory_exist(factory=self._features_factory)
+            self._init_queue_process(tasks=queue_tasks)
+
+        # # Filter mechanism
+        __lock_features = filter(lambda __feature: __feature not in [Feature.Event, Feature.Condition], features)
+        __communication_features = filter(lambda __feature: __feature in [Feature.Event, Feature.Condition], features)
+
+        # # Lock initialization
+        if __lock_features:
+            self._features_factory_exist(factory=self._features_factory)
+            self._init_lock_process(features=__lock_features)
+
+        # # Communication initialization
+        if __communication_features:
+            self._features_factory_exist(factory=self._features_factory)
+            self._init_communication_process(features=__communication_features)
+
+
+    def _features_factory_exist(self, factory: BaseFeatureAdapterFactory, force: bool = True) -> bool:
+        if factory is not None:
+            return True
+        else:
+            if force is True:
+                raise FeatureFactoryCannotBeEmpty
+            else:
+                return False
+
+
+    def _init_queue_process(self, tasks: List[BaseQueueTask]) -> None:
+        """
+        Initialize Queue object which be needed to handle in Queue-Task-List.
+        :param tasks:
+        :return:
+        """
+
+        __queue_adapter = self._features_factory.get_queue_adapter()
+        # __queue_adapter = QueueAdapter(mode=self._Running_Feature_Mode)
+        for task in tasks:
+            __queue = __queue_adapter.init_queue_with_values(qtype=task.queue_type, values=task.value)
+            RunningGlobalize.queue(name=task.name, queue=__queue)
+
+
+    def _init_lock_process(self, features: Iterable[Feature], **adapter_kwargs) -> None:
+        """
+        Initialize Lock object (Lock, RLock, Semaphore, Bounded Semaphore)
+        which be needed to handle in Feature-List.
+        :param features:
+        :return:
+        """
+
+        __lock_adapter = self._features_factory.get_lock_adapter(**adapter_kwargs)
+        # __lock_adapter = LockAdapter(mode=self._Running_Feature_Mode, **adapter_kwargs)
+        for feature in features:
+            if feature == Feature.Lock:
+                __lock = __lock_adapter.get_lock()
+                RunningGlobalize.lock(lock=__lock)
+            if feature == Feature.RLock:
+                __rlock = __lock_adapter.get_rlock()
+                RunningGlobalize.rlock(rlock=__rlock)
+            if feature == Feature.Semaphore:
+                __semaphore = __lock_adapter.get_semaphore(value=self.db_connection_number)
+                RunningGlobalize.semaphore(smp=__semaphore)
+            if feature == Feature.Bounded_Semaphore:
+                __bounded_semaphore = __lock_adapter.get_bounded_semaphore(value=self.db_connection_number)
+                RunningGlobalize.bounded_semaphore(bsmp=__bounded_semaphore)
+
+
+    def _init_communication_process(self, features: Iterable[Feature], **adapter_kwargs) -> None:
+        """
+        Initialize Lock object (Event, Condition) which be needed to
+        handle in Feature-List.
+        :param features:
+        :return:
+        """
+
+        __communication_adapter = self._features_factory.get_communication_adapter(**adapter_kwargs)
+        # __communication_adapter = CommunicationAdapter(mode=self._Running_Feature_Mode, **adapter_kwargs)
+        for feature in features:
+            if feature == feature.Event:
+                __event = __communication_adapter.get_event()
+                RunningGlobalize.event(event=__event)
+            if feature == feature.Condition:
+                __condition = __communication_adapter.get_condition()
+                RunningGlobalize.condition(condition=__condition)
+
+
+
 class AsyncRunnableStrategy(RunnableStrategy, ABC):
 
     async def initialization(self, queue_tasks: Optional[List[BaseQueueTask]] = None,
                              features: Optional[List[Feature]] = None, *args, **kwargs) -> None:
-        pass
+        """
+        Description:
+            Asynchronous version of method 'initialization'.
+        :param queue_tasks:
+        :param features:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        # # Queue initialization
+        if queue_tasks is not None:
+            self._features_factory_exist(factory=self._features_factory)
+            await self._init_queue_process(tasks=queue_tasks)
+
+        # # Filter mechanism
+        __lock_features = filter(lambda __feature: __feature not in [Feature.Event, Feature.Condition], features)
+        __communication_features = filter(lambda __feature: __feature in [Feature.Event, Feature.Condition], features)
+
+        __async_kwgs = {}
+        __event_loop = kwargs.get("event_loop", None)
+        if __event_loop is None:
+            raise Exception("Async Event Loop object cannot be empty.")
+        __async_kwgs["event_loop"] = __event_loop
+
+        # # Lock initialization
+        if __lock_features:
+            self._features_factory_exist(factory=self._features_factory)
+            super()._init_lock_process(features=features, **__async_kwgs)
+
+        # # Communication initialization
+        if __communication_features:
+            self._features_factory_exist(factory=self._features_factory)
+            super()._init_communication_process(features=features, **__async_kwgs)
+
+
+    async def _init_queue_process(self, tasks: List[BaseQueueTask]) -> None:
+        """
+        Description:
+            Asynchronous version of method '_init_queue_process'.
+        :param tasks:
+        :return:
+        """
+
+        __queue_adapter = self._features_factory.get_queue_adapter()
+        # __queue_adapter = QueueAdapter(mode=self._Running_Feature_Mode)
+        for task in tasks:
+            __queue = await __queue_adapter.async_init_queue_with_values(qtype=task.queue_type, values=task.value)
+            RunningGlobalize.queue(name=task.name, queue=__queue)
 
 
     @abstractmethod
     async def build_workers(self, function: Callable, *args, **kwargs) -> List[OceanTasks]:
         """
         Description:
-            Assign tasks into each different threads or processes.
+            Asynchronous version of method 'build_workers'.
         :param function:
         :param args:
         :param kwargs:
@@ -202,7 +312,7 @@ class AsyncRunnableStrategy(RunnableStrategy, ABC):
     async def activate_workers(self, workers_list: List[OceanTasks]) -> None:
         """
         Description:
-            Activate multiple threads or processes to run target task(s).
+            Asynchronous version of method 'activate_workers'.
         :param workers_list:
         :return:
         """
@@ -213,7 +323,7 @@ class AsyncRunnableStrategy(RunnableStrategy, ABC):
     async def close(self) -> None:
         """
         Description:
-            The final in procedure which the progeram should be run.
+            Asynchronous version of method 'close'.
         :return:
         """
         pass
