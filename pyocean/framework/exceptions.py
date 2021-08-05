1ac941d50775c58b710d03f3b62e3974a7eeabe3
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-05 23:20:22 +0800

    Add a new RunningError.

--- pyocean/framework/exceptions.py
+++ pyocean/framework/exceptions.py
@@ -7,3 +7,11 @@ class ParameterCannotBeEmpty(Exception):
 
     def __str__(self):
         return f"The parameter '{self.__param} shouldn't be empty.'"
+
+
+
+class FeatureFactoryCannotBeEmpty(RuntimeError):
+
+    def __str__(self):
+        return "Factory object for feature-adapter cannot be empty."
+
