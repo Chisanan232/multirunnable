38e785ce8df7583781fbb55cdd57d3ef302f350c
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-23 23:28:03 +0800

    Add import-path of modules for lock, communication and queue.

--- pyocean/adapter/__init__.py
+++ pyocean/adapter/__init__.py
@@ -1,6 +1,11 @@
 from pyocean.adapter.features_adapter import (
+    Lock, RLock, Semaphore, BoundedSemaphore, Event, Condition,
     FeatureAdapterFactory,
     QueueAdapter,
     LockAdapter,
     CommunicationAdapter)
+from pyocean.adapter.queue import Queue
+from pyocean.adapter.lock import Lock, RLock, Semaphore, BoundedSemaphore
+from pyocean.adapter.communication import Event, Condition
+from pyocean.adapter.collection import FeatureList, QueueTaskList
 from pyocean.adapter.strategy_adapter import StrategyAdapter
