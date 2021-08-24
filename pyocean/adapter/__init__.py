01615ad4ce36a730482feb645e581b32991ed22a
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-24 11:51:46 +0800

    Modify the module naming.

--- pyocean/adapter/__init__.py
+++ pyocean/adapter/__init__.py
@@ -2,4 +2,4 @@ from pyocean.adapter.queue import Queue
 from pyocean.adapter.lock import Lock, RLock, Semaphore, BoundedSemaphore
 from pyocean.adapter.communication import Event, Condition
 from pyocean.adapter.collection import FeatureList, QueueTaskList
-from pyocean.adapter.strategy_adapter import StrategyAdapter
+from pyocean.adapter.strategy import StrategyAdapter
