bd79a785b00e9be922769f7187baf921b5379efa
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-24 11:12:03 +0800

    Remove useless code.

--- pyocean/adapter/queue.py
+++ pyocean/adapter/queue.py
@@ -4,47 +4,21 @@ from pyocean.mode import FeatureMode
 from pyocean.api.manager import Globalize
 from pyocean.types import OceanQueue
 from pyocean.adapter.base import QueueAdapterFactory, BaseAdapter
-# from pyocean.adapter.collection import QueueTaskList
-# from pyocean.adapter._utils import _ModuleFactory
 from pyocean._import_utils import ImportPyocean
 
-from typing import List, Iterable, Any
+from typing import Iterable, Any
 
 
 
 class Queue(QueueAdapterFactory):
 
-    def __init__(self, **kwargs):
-        super(Queue, self).__init__(**kwargs)
-
-        self.__name = kwargs.get("name", None)
-        if self.__name is None:
-            raise Exception("The name of Queue object shouldn't be None object. "
-                            "It's the key of each Queue object you create.")
-
-        self.__qtype = kwargs.get("qtype", None)
-        if self.__qtype is None:
-            raise Exception("Queue type parameter shouldn't be None object. "
-                            "It must to choice a type of Queue object with mapping strategy.")
-
-
-    def __str__(self):
-        return f"<Queue Object at {id(self)}>"
-
-
-    def __repr__(self):
-        return f"<Queue Object(name={self.__name}, qtype={self.__qtype}) ar {id(self)}>"
-
-
     def get_instance(self) -> OceanQueue:
-        __queue = self.__qtype.value
+        __queue = self._qtype.value
         return __queue
-        # queue_instance: BaseQueue = _ModuleFactory.get_queue_adapter(mode=self._mode)
-        # return queue_instance.get_queue(qtype=self.__qtype)
 
 
     def globalize_instance(self, obj) -> None:
-        Globalize.queue(name=self.__name, queue=obj)
+        Globalize.queue(name=self._name, queue=obj)
 
 
 
