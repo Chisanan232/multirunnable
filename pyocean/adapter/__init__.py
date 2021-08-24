81d23fc5dab5d822b299e973f6fbc4f987fd19ef
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-24 11:46:47 +0800

    Modify the __init__ of adapter module.

--- pyocean/adapter/__init__.py
+++ pyocean/adapter/__init__.py
@@ -1,9 +1,3 @@
-from pyocean.adapter.features_adapter import (
-    Lock, RLock, Semaphore, BoundedSemaphore, Event, Condition,
-    FeatureAdapterFactory,
-    QueueAdapter,
-    LockAdapter,
-    CommunicationAdapter)
 from pyocean.adapter.queue import Queue
 from pyocean.adapter.lock import Lock, RLock, Semaphore, BoundedSemaphore
 from pyocean.adapter.communication import Event, Condition
