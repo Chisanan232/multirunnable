d17c458cfe93a8f2a5c260b7f5990aaf97a83272
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-28 17:47:49 +0800

    Add asynchronous version of 'retry' decorator.

--- pyocean/api/decorator.py
+++ pyocean/api/decorator.py
@@ -7,6 +7,7 @@ from pyocean.api.operator import (
 
 from functools import wraps, update_wrapper, partial
 from typing import List, Tuple, Dict, Callable, Type, Any, Union
+import inspect
 
 
 
@@ -335,6 +336,149 @@ class _Retry:
         def __wrapper(func_result):
             pass
         return __wrapper
+
+
+
+class AsyncReTryDefaultFunction:
+
+    async def initial(self, *args, **kwargs):
+        pass
+
+
+    async def error_handling(self, e: Exception):
+        raise e
+
+
+    async def done_handling(self, result):
+        return result
+
+
+    async def final_handling(self):
+        pass
+
+
+
+def async_retry(function: Callable = None, timeout: int = 1):
+
+    if function:
+        return _AsyncRetry(function=function, timeout=timeout)
+    else:
+        @wraps(function)
+        def __async_retry(function: Callable):
+            return _AsyncRetry(function=function, timeout=timeout)
+        return __async_retry
+
+
+
+class _AsyncRetry:
+
+    __Running_Timeout: int = None
+
+    __default_func = AsyncReTryDefaultFunction()
+
+    __Target_Function: Callable = None
+    __Initial_Function: Callable = __default_func.initial
+    __Initial_Args: Tuple = ()
+    __Initial_Kwargs: Dict = {}
+    __Exception_Handling_Function: Callable = __default_func.error_handling
+    __Done_Handling_Function: Callable = __default_func.done_handling
+    __Final_Handling_Function: Callable = __default_func.final_handling
+
+    FunctionNotCoroutineError = TypeError("The marker decorator function isn't coroutine.")
+
+
+    def __init__(self, function: Callable, timeout: int = 1):
+        self.__chk_coroutine_function(function=function)
+        update_wrapper(self, function)
+        self.__Target_Function = function
+        self.__Running_Timeout = timeout
+
+
+    def __get__(self, instance, owner):
+        return partial(self.__call__, instance)
+
+
+    def __call__(self, instance, *args, **kwargs):
+        __running_counter = 0
+        __result = None
+
+        while __running_counter < self.__Running_Timeout:
+            try:
+                await self.__Initial_Function(*self.__Initial_Args, **self.__Initial_Kwargs)
+                __result = await self.__Target_Function(instance, *args, **kwargs)
+            except Exception as e:
+                __error_handling_result = await self.__Exception_Handling_Function(e)
+                if __error_handling_result:
+                    __result = __error_handling_result
+                else:
+                    __result = e
+            else:
+                __result = await self.__Done_Handling_Function(__result)
+            finally:
+                await self.__Final_Handling_Function()
+                __running_counter += 1
+        else:
+            __result = TimeoutError("The target function running timeout.")
+
+        return __result
+
+
+    @classmethod
+    def initialization(cls, function: Callable):
+        cls.__chk_coroutine_function(function=function)
+        cls.__Initial_Function = function
+        __self = cls
+
+        @wraps(function)
+        def __wrapper(*args, **kwargs):
+            __self.__Initial_Args = args
+            __self.__Initial_Kwargs = kwargs
+
+        return __wrapper
+
+
+    @classmethod
+    def error_handling(cls, function: Callable):
+        cls.__chk_coroutine_function(function=function)
+        cls.__Exception_Handling_Function = function
+        __self = cls
+
+        @wraps(function)
+        def __wrapper(e: Exception):
+            pass
+        return __wrapper
+
+
+    @classmethod
+    def done_handling(cls, function):
+        cls.__chk_coroutine_function(function=function)
+        cls.__Done_Handling_Function = function
+        __self = cls
+
+        @wraps(function)
+        def __wrapper(func_result):
+            pass
+        return __wrapper
+
+
+    @classmethod
+    def final_handling(cls, function):
+        cls.__chk_coroutine_function(function=function)
+        cls.__Final_Handling_Function = function
+
+        @wraps(function)
+        def __wrapper(func_result):
+            pass
+        return __wrapper
+
+
+    @classmethod
+    def __chk_coroutine_function(cls, function: Callable):
+        __chksum = inspect.iscoroutinefunction(function)
+        if __chksum is True:
+            return True
+        else:
+            raise cls.FunctionNotCoroutineError
 
 
 
