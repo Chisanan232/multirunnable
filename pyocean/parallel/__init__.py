bcf12c176b83492dc5ce42ef7c26ab70ef3812d7
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-31 22:59:02 +0800

    Modify the Strategy naming from 'MultiProcessing' to 'ProcessPool'.

--- pyocean/parallel/__init__.py
+++ pyocean/parallel/__init__.py
@@ -1,3 +1,3 @@
 from pyocean.parallel.features import MultiProcessingQueueType, ProcessLock, ProcessCommunication
-from pyocean.parallel.strategy import ParallelStrategy, MultiProcessingStrategy
+from pyocean.parallel.strategy import ParallelStrategy, ProcessPoolStrategy
 from pyocean.parallel.result import ParallelResult
