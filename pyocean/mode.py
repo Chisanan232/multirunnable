ae771c2bf756f1a3b1a21569bccb3f0cfb67a755
Chisanan232 <chi10211201@cycu.org.tw>
2021-08-31 22:55:59 +0800

    Modify the variable naming to be clear and add new moduel 'MapStrategy'.

--- pyocean/mode.py
+++ pyocean/mode.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 
 _Package: str = "pyocean"
-_Parallel_Module: str = ".parallel."
-_Concurrent_Module: str = ".concurrent."
-_Coroutine_Module: str = ".coroutine."
-_Feature_Module: str = "features"
-_Strategy_Module: str = "strategy"
+_Parallel_Package: str = ".parallel."
+_Concurrent_Package: str = ".concurrent."
+_Coroutine_Package: str = ".coroutine."
+_Feature_Package: str = "features"
+_Strategy_Package: str = "strategy"
 
 
 # # Feature class
@@ -22,39 +22,42 @@ _Communication_Class: str = "Communication"
 
 # # Strategy class
 _Parallel_Common_Class: str = "MultiProcessing"
+_Process_Pool_Common_Class: str = "ProcessPool"
+_Processes_Common_Class: str = "MultiProcesses"
 _Concurrent_Common_Class: str = "MultiThreading"
 _Greenlet_Common_Class: str = "MultiGreenlet"
 _Async_Common_Class: str = "Asynchronous"
 
 _Strategy_Class: str = "Strategy"
+_Map_Strategy_Class: str = "MapStrategy"
 
 
 
 class FeatureMode(Enum):
 
     Parallel = {
-        "module": _Parallel_Module + _Feature_Module,
+        "module": _Parallel_Package + _Feature_Package,
         "queue": _Parallel_Feature_Class + _Queue_Class,
         "lock": _Parallel_Feature_Class + _Lock_Class,
         "communication": _Parallel_Feature_Class + _Communication_Class
     }
 
     Concurrent = {
-        "module": _Concurrent_Module + _Feature_Module,
+        "module": _Concurrent_Package + _Feature_Package,
         "queue": _Concurrent_Feature_Class + _Queue_Class,
         "lock": _Concurrent_Feature_Class + _Lock_Class,
         "communication": _Concurrent_Feature_Class + _Communication_Class
     }
 
     Greenlet = {
-        "module": _Coroutine_Module + _Feature_Module,
+        "module": _Coroutine_Package + _Feature_Package,
         "queue": _Greenlet_Feature_Class + _Queue_Class,
         "lock": _Greenlet_Feature_Class + _Lock_Class,
         "communication": _Greenlet_Feature_Class + _Communication_Class
     }
 
     Asynchronous = {
-        "module": _Coroutine_Module + _Feature_Module,
+        "module": _Coroutine_Package + _Feature_Package,
         "queue": _Async_Feature_Class + _Queue_Class,
         "lock": _Async_Feature_Class + _Lock_Class,
         "communication": _Async_Feature_Class + _Communication_Class
@@ -65,22 +68,32 @@ class FeatureMode(Enum):
 class RunningMode(Enum):
 
     Parallel = {
-        "strategy_module": _Parallel_Module + _Strategy_Module,
-        "strategy": _Parallel_Common_Class + _Strategy_Class
+        "strategy_module": _Parallel_Package + _Strategy_Package,
+        "strategy": _Process_Pool_Common_Class + _Strategy_Class,
+        "map_strategy": _Parallel_Common_Class + _Map_Strategy_Class
+    }
+
+    MultiProcesses = {
+        "strategy_module": _Parallel_Package + _Strategy_Package,
+        "strategy": _Processes_Common_Class + _Strategy_Class,
+        "map_strategy": _Parallel_Common_Class + _Map_Strategy_Class
     }
 
     Concurrent = {
-        "strategy_module": _Concurrent_Module + _Strategy_Module,
-        "strategy": _Concurrent_Common_Class + _Strategy_Class
+        "strategy_module": _Concurrent_Package + _Strategy_Package,
+        "strategy": _Concurrent_Common_Class + _Strategy_Class,
+        "map_strategy": _Concurrent_Common_Class + _Map_Strategy_Class
     }
 
     Greenlet = {
-        "strategy_module": _Coroutine_Module + _Strategy_Module,
-        "strategy": _Greenlet_Common_Class + _Strategy_Class
+        "strategy_module": _Coroutine_Package + _Strategy_Package,
+        "strategy": _Greenlet_Common_Class + _Strategy_Class,
+        "map_strategy": _Greenlet_Common_Class + _Map_Strategy_Class
     }
 
     Asynchronous = {
-        "strategy_module": _Coroutine_Module + _Strategy_Module,
-        "strategy": _Async_Common_Class + _Strategy_Class
+        "strategy_module": _Coroutine_Package + _Strategy_Package,
+        "strategy": _Async_Common_Class + _Strategy_Class,
+        "map_strategy": _Async_Common_Class + _Map_Strategy_Class
     }
 
