c79a6d81984ebb05899482cd7b1276e8a03d62ed
Chisanan232 <chi10211201@cycu.org.tw>
2021-09-01 23:53:06 +0800

    Modify the signature of implementation about Concurrent.

--- pyocean/concurrent/strategy.py
+++ pyocean/concurrent/strategy.py
@@ -1,7 +1,10 @@
 from pyocean.framework.task import BaseTask as _BaseTask, BaseQueueTask as _BaseQueueTask
 from pyocean.framework.features import BaseFeatureAdapterFactory as _BaseFeatureAdapterFactory
 from pyocean.framework.adapter.collection import BaseList as _BaseList
-from pyocean.framework.strategy import RunnableStrategy as _RunnableStrategy, Resultable as _Resultable
+from pyocean.framework.strategy import (
+    RunnableStrategy as _RunnableStrategy,
+    BaseMapStrategy as _BaseMapStrategy,
+    Resultable as _Resultable)
 from pyocean.mode import RunningMode as _RunningMode, FeatureMode as _FeatureMode
 from pyocean.task import OceanTask as _OceanTask
 from pyocean.concurrent.result import ConcurrentResult as _ConcurrentResult
@@ -9,7 +12,10 @@ from pyocean.exceptions import FunctionSignatureConflictError as _FunctionSignat
 from pyocean.concurrent.exceptions import ThreadsListIsEmptyError as _ThreadsListIsEmptyError
 
 from abc import abstractmethod, ABC
-from typing import List, Dict, Callable, Optional, Union
+from typing import List, Dict, Callable, Iterable as IterableType, Optional, Union, Tuple
+from types import MethodType
+from collections import Iterable
+from multipledispatch import dispatch
 from functools import wraps
 from threading import Thread
 
@@ -22,14 +28,9 @@ class ConcurrentStrategy(_RunnableStrategy, ABC):
     _Threads_Running_Result: Dict[str, Dict[str, Union[object, bool]]] = {}
     _Threading_Running_Result: List = []
 
-    def activate_workers(self, workers_list: List[Thread]) -> None:
-        # # Method 1.
-        for worker in workers_list:
+    def activate_workers(self) -> None:
+        for worker in self._Threads_List:
             self.activate_worker(worker=worker)
-
-        # # Method 2.
-        # with workers_list as worker:
-        #     self.activate_worker(worker=worker)
 
 
     @abstractmethod
@@ -43,15 +44,15 @@ class ConcurrentStrategy(_RunnableStrategy, ABC):
         pass
 
 
-    @staticmethod
-    def save_return_value(function: Callable) -> Callable:
+    @classmethod
+    def save_return_value(cls, function: Callable) -> Callable:
+        __self = cls
 
         @wraps(function)
-        def save_value_fun(self, task: _BaseTask) -> None:
-            self = self
-            value = task.function(*task.func_args, **task.func_kwargs)
+        def save_value_fun(*args, **kwargs) -> None:
+            value = function(*args, **kwargs)
             __thread_result = {"result": value}
-            self._Threading_Running_Result.append(__thread_result)
+            __self._Threading_Running_Result.append(__thread_result)
 
         return save_value_fun
 
@@ -70,41 +71,14 @@ class MultiThreadingStrategy(ConcurrentStrategy, _Resultable):
             self._persistence_strategy.initialize(db_conn_num=self.db_connection_number)
 
 
-    def build_workers(self, function: Callable, *args, **kwargs) -> List[Thread]:
+    def build_workers(self, function: Callable, *args, **kwargs) -> None:
 
-        def __general_task() -> _OceanTask:
-            __otask = _OceanTask(mode=_RunningMode.Concurrent)
-            __otask.set_function(function=function)
-            __otask.set_func_args(args=args)
-            __otask.set_func_kwargs(kwargs=kwargs)
-            return __otask
+        @ConcurrentStrategy.save_return_value
+        def target_task(*args, **kwargs) -> None:
+            value = function(*args, **kwargs)
+            return value
 
-        if args:
-            __task = filter(lambda arg: isinstance(arg, _BaseTask), args)
-            if __task is None:
-                __task = __general_task()
-            __args = (__task,)
-        else:
-            __args = ()
-
-        if kwargs:
-            __task = kwargs.get("task", None)
-            if __task is None:
-                __task = __general_task()
-            else:
-                if not isinstance(__task, _BaseTask):
-                    raise _FunctionSignatureConflictError
-            __kwargs = {"task": __task}
-        else:
-            __kwargs = {}
-
-        self._Threads_List = [Thread(target=self.target_task, args=__args, kwargs=__kwargs) for _ in range(self.workers_number)]
-        return self._Threads_List
-
-
-    @ConcurrentStrategy.save_return_value
-    def target_task(self, task: _BaseTask) -> None:
-        task.function(self, *task.func_args, **task.func_kwargs)
+        self._Threads_List = [Thread(target=target_task, args=args, kwargs=kwargs) for _ in range(self.workers_number)]
 
 
     def activate_worker(self, worker: Thread) -> None:
@@ -133,3 +107,65 @@ class MultiThreadingStrategy(ConcurrentStrategy, _Resultable):
             __concurrent_results.append(__concurrent_result)
 
         return __concurrent_results
+
+
+
+class MultiThreadingMapStrategy(_BaseMapStrategy, _Resultable):
+
+    _Strategy_Feature_Mode: _FeatureMode = _FeatureMode.Concurrent
+
+    def initialization(self,
+                       queue_tasks: Optional[Union[_BaseQueueTask, _BaseList]] = None,
+                       features: Optional[Union[_BaseFeatureAdapterFactory, _BaseList]] = None,
+                       *args, **kwargs) -> None:
+        super(MultiThreadingMapStrategy, self).initialization(
+            queue_tasks=queue_tasks, features=features, *args, **kwargs)
+
+        # # Persistence
+        # if self._persistence_strategy is not None:
+        #     self._persistence_strategy.initialize(db_conn_num=self.db_connection_number)
+
+
+    def generate_worker(self, target: Callable, args: Tuple = (), kwargs: Dict = {}) -> Thread:
+        return Thread(target=target, args=args, kwargs=kwargs)
+
+
+    @dispatch(Thread)
+    def activate_worker(self, workers: Thread) -> None:
+        workers.start()
+
+
+    @dispatch(Iterable)
+    def activate_worker(self, workers: List[Thread]) -> None:
+        for worker in workers:
+            self.activate_worker(worker)
+
+
+    @dispatch(Thread)
+    def close_worker(self, workers: Thread) -> None:
+        workers.join()
+
+
+    @dispatch(Iterable)
+    def close_worker(self, workers: List[Thread]) -> None:
+        for worker in workers:
+            self.close_worker(worker)
+
+
+    @dispatch(MethodType, tuple, dict)
+    def start_new_worker(self, target: Callable, args: Tuple = (), kwargs: Dict = {}) -> Thread:
+        __worker = self.generate_worker(target=target, *args, **kwargs)
+        self.activate_worker(__worker)
+        return __worker
+
+
+    @dispatch(Iterable, tuple, dict)
+    def start_new_worker(self, target: List[Callable], args: Tuple = (), kwargs: Dict = {}) -> List[Thread]:
+        __workers = [self.generate_worker(target=__function, *args, **kwargs) for __function in target]
+        self.activate_worker(__workers)
+        return __workers
+
+
+    def get_result(self) -> IterableType[object]:
+        pass
+
