1fc354a0fd325aa3122981ceb502491b08bf1c22
Chisanan232 <chi10211201@cycu.org.tw>
2021-09-05 23:30:50 +0800

    Implement 'executor' and 'pool' moduels.

--- pyocean/concurrent/strategy.py
+++ pyocean/concurrent/strategy.py
@@ -1,48 +1,33 @@
-from pyocean.framework.task import BaseTask as _BaseTask, BaseQueueTask as _BaseQueueTask
+from pyocean.framework import OceanResult as _OceanResult
+from pyocean.framework.task import (
+    BaseQueueTask as _BaseQueueTask,
+    BasePersistenceTask as _BasePersistenceTask)
 from pyocean.framework.features import BaseFeatureAdapterFactory as _BaseFeatureAdapterFactory
 from pyocean.framework.adapter.collection import BaseList as _BaseList
 from pyocean.framework.strategy import (
-    RunnableStrategy as _RunnableStrategy,
-    BaseMapStrategy as _BaseMapStrategy,
+    GeneralRunnableStrategy as _GeneralRunnableStrategy,
+    PoolRunnableStrategy as _PoolRunnableStrategy,
     Resultable as _Resultable)
-from pyocean.mode import RunningMode as _RunningMode, FeatureMode as _FeatureMode
-from pyocean.task import OceanTask as _OceanTask
+from pyocean.mode import FeatureMode as _FeatureMode
 from pyocean.concurrent.result import ConcurrentResult as _ConcurrentResult
-from pyocean.exceptions import FunctionSignatureConflictError as _FunctionSignatureConflictError
-from pyocean.concurrent.exceptions import ThreadsListIsEmptyError as _ThreadsListIsEmptyError
 
-from abc import abstractmethod, ABC
-from typing import List, Dict, Callable, Iterable as IterableType, Optional, Union, Tuple
+from typing import List, Dict, Callable, Iterable as IterableType, Optional, Union, Tuple, cast
 from types import MethodType
 from collections import Iterable
 from multipledispatch import dispatch
 from functools import wraps
+from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import AsyncResult, ApplyResult
 from threading import Thread
 
+from pyocean.types import OceanTasks as _OceanTasks
 
 
-class ConcurrentStrategy(_RunnableStrategy, ABC):
+class ConcurrentStrategy:
 
     _Strategy_Feature_Mode = _FeatureMode.Concurrent
-    _Threads_List: List[Thread] = []
-    _Threads_Running_Result: Dict[str, Dict[str, Union[object, bool]]] = {}
     _Threading_Running_Result: List = []
-
-    def activate_workers(self) -> None:
-        for worker in self._Threads_List:
-            self.activate_worker(worker=worker)
-
-
-    @abstractmethod
-    def activate_worker(self, worker: Thread) -> None:
-        """
-        Description:
-            Each one thread or process running task implementation.
-        :param worker:
-        :return:
-        """
-        pass
-
+    _Thread_Running_Result: List[Dict[str, Union[AsyncResult, bool]]] = []
 
     @classmethod
     def save_return_value(cls, function: Callable) -> Callable:
@@ -57,50 +42,14 @@ class ConcurrentStrategy(_RunnableStrategy, ABC):
         return save_value_fun
 
 
-
-class MultiThreadingStrategy(ConcurrentStrategy, _Resultable):
-
-    def initialization(self,
-                       queue_tasks: Optional[Union[_BaseQueueTask, _BaseList]] = None,
-                       features: Optional[Union[_BaseFeatureAdapterFactory, _BaseList]] = None,
-                       *args, **kwargs) -> None:
-        super(MultiThreadingStrategy, self).initialization(queue_tasks=queue_tasks, features=features, *args, **kwargs)
-
-        # # Persistence
-        if self._persistence_strategy is not None:
-            self._persistence_strategy.initialize(db_conn_num=self.db_connection_number)
-
-
-    def build_workers(self, function: Callable, *args, **kwargs) -> None:
-
-        @ConcurrentStrategy.save_return_value
-        def target_task(*args, **kwargs) -> None:
-            value = function(*args, **kwargs)
-            return value
-
-        self._Threads_List = [Thread(target=target_task, args=args, kwargs=kwargs) for _ in range(self.workers_number)]
-
-
-    def activate_worker(self, worker: Thread) -> None:
-        worker.start()
-
-
-    def close(self) -> None:
-        if self._Threads_List:
-            for threed_index in range(self.workers_number):
-                self._Threads_List[threed_index].join()
-        else:
-            raise _ThreadsListIsEmptyError
-
-
-    def get_result(self) -> List[_ConcurrentResult]:
+    def result(self) -> List[_ConcurrentResult]:
         __concurrent_result = self._result_handling()
         return __concurrent_result
 
 
     def _result_handling(self) -> List[_ConcurrentResult]:
         __concurrent_results = []
-        for __result in self._Threading_Running_Result:
+        for __result in self._Thread_Running_Result:
             __concurrent_result = _ConcurrentResult()
             __concurrent_result.data = __result.get("result")
 
@@ -110,62 +59,251 @@ class MultiThreadingStrategy(ConcurrentStrategy, _Resultable):
 
 
 
-class MultiThreadingMapStrategy(_BaseMapStrategy, _Resultable):
+class ThreadStrategy(_GeneralRunnableStrategy, ConcurrentStrategy, _Resultable):
 
     _Strategy_Feature_Mode: _FeatureMode = _FeatureMode.Concurrent
-
-    def initialization(self,
-                       queue_tasks: Optional[Union[_BaseQueueTask, _BaseList]] = None,
-                       features: Optional[Union[_BaseFeatureAdapterFactory, _BaseList]] = None,
-                       *args, **kwargs) -> None:
-        super(MultiThreadingMapStrategy, self).initialization(
-            queue_tasks=queue_tasks, features=features, *args, **kwargs)
-
-        # # Persistence
-        # if self._persistence_strategy is not None:
-        #     self._persistence_strategy.initialize(db_conn_num=self.db_connection_number)
-
-
-    def generate_worker(self, target: Callable, args: Tuple = (), kwargs: Dict = {}) -> Thread:
-        return Thread(target=target, args=args, kwargs=kwargs)
-
-
-    @dispatch(Thread)
-    def activate_worker(self, workers: Thread) -> None:
-        workers.start()
-
-
-    @dispatch(Iterable)
-    def activate_worker(self, workers: List[Thread]) -> None:
-        for worker in workers:
-            self.activate_worker(worker)
-
-
-    @dispatch(Thread)
-    def close_worker(self, workers: Thread) -> None:
-        workers.join()
-
-
-    @dispatch(Iterable)
-    def close_worker(self, workers: List[Thread]) -> None:
-        for worker in workers:
-            self.close_worker(worker)
-
+    __Thread_List: List[Thread] = None
 
     @dispatch(MethodType, tuple, dict)
     def start_new_worker(self, target: Callable, args: Tuple = (), kwargs: Dict = {}) -> Thread:
         __worker = self.generate_worker(target=target, *args, **kwargs)
-        self.activate_worker(__worker)
+        self.activate_workers(__worker)
         return __worker
 
 
     @dispatch(Iterable, tuple, dict)
     def start_new_worker(self, target: List[Callable], args: Tuple = (), kwargs: Dict = {}) -> List[Thread]:
         __workers = [self.generate_worker(target=__function, *args, **kwargs) for __function in target]
-        self.activate_worker(__workers)
+        self.activate_workers(__workers)
         return __workers
 
 
-    def get_result(self) -> IterableType[object]:
+    def generate_worker(self, target: Callable, *args, **kwargs) -> _OceanTasks:
+        return Thread(target=target, args=args, kwargs=kwargs)
+
+
+    @dispatch(Thread)
+    def activate_workers(self, workers: Thread) -> None:
+        workers.start()
+
+
+    @dispatch(Iterable)
+    def activate_workers(self, workers: List[Thread]) -> None:
+        for worker in workers:
+            self.activate_workers(worker)
+
+
+    @dispatch(Thread)
+    def close(self, workers: Thread) -> None:
+        workers.join()
+
+
+    @dispatch(Iterable)
+    def close(self, workers: List[Thread]) -> None:
+        for worker in workers:
+            self.close(worker)
+
+
+    def kill(self) -> None:
         pass
+
+
+    def terminal(self) -> None:
+        pass
+
+
+    def get_result(self) -> List[_OceanResult]:
+        pass
+
+
+
+class ThreadPoolStrategy(_PoolRunnableStrategy, ConcurrentStrategy, _Resultable):
+
+    _Thread_Pool: ThreadPool = None
+    _Thread_List: List[Union[ApplyResult, AsyncResult]] = None
+
+    def __init__(self, pool_size: int, tasks_size: int, persistence: _BasePersistenceTask = None):
+        super().__init__(pool_size=pool_size, tasks_size=tasks_size, persistence=persistence)
+        # self._init_namespace_obj()
+        # if persistence is not None:
+        #     namespace_persistence = cast(_BasePersistenceTask, self.namespacing_obj(obj=persistence))
+        #     # super().__init__(persistence=namespace_persistence)
+        #     self._persistence = namespace_persistence
+        # self._Processors_Running_Result = self._Manager.list()
+
+
+    def initialization(self,
+                       queue_tasks: Optional[Union[_BaseQueueTask, _BaseList]] = None,
+                       features: Optional[Union[_BaseFeatureAdapterFactory, _BaseList]] = None,
+                       *args, **kwargs) -> None:
+        super(ThreadPoolStrategy, self).initialization(queue_tasks=queue_tasks, features=features, *args, **kwargs)
+
+        # # Persistence
+        if self._persistence_strategy is not None:
+            self._persistence_strategy.initialize(db_conn_num=self.db_connection_pool_size)
+
+        # Initialize and build the Processes Pool.
+        __pool_initializer: Callable = kwargs.get("pool_initializer", None)
+        __pool_initargs: IterableType = kwargs.get("pool_initargs", None)
+        self._Thread_Pool = ThreadPool(processes=self.pool_size, initializer=__pool_initializer, initargs=__pool_initargs)
+
+
+    def apply(self, function: Callable, *args, **kwargs) -> None:
+        __process_running_result = None
+
+        try:
+            __process_running_result = [
+                self._Thread_Pool.apply(func=function, args=args, kwds=kwargs)
+                for _ in range(self.tasks_size)]
+            __exception = None
+            __process_run_successful = True
+        except Exception as e:
+            __exception = e
+            __process_run_successful = False
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def async_apply(self,
+                    function: Callable,
+                    args: Tuple = (),
+                    kwargs: Dict = {},
+                    callback: Callable = None,
+                    error_callback: Callable = None) -> None:
+        self._Thread_List = [
+            self._Thread_Pool.apply_async(func=function,
+                                          args=args,
+                                          kwds=kwargs,
+                                          callback=callback,
+                                          error_callback=error_callback)
+            for _ in range(self.tasks_size)]
+
+        for process in self._Thread_List:
+            __process_running_result = process.get()
+            __process_run_successful = process.successful()
+
+            # Save Running result state and Running result value as dict
+            self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def map(self, function: Callable, args_iter: IterableType = (), chunksize: int = None) -> None:
+        __process_running_result = None
+
+        try:
+            __process_running_result = self._Thread_Pool.map(
+                func=function, iterable=args_iter, chunksize=chunksize)
+            __exception = None
+            __process_run_successful = True
+        except Exception as e:
+            __exception = e
+            __process_run_successful = False
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def async_map(self,
+                  function: Callable,
+                  args_iter: IterableType = (),
+                  chunksize: int = None,
+                  callback: Callable = None,
+                  error_callback: Callable = None) -> None:
+        __map_result = self._Thread_Pool.map_async(
+            func=function,
+            iterable=args_iter,
+            chunksize=chunksize,
+            callback=callback,
+            error_callback=error_callback)
+        __process_running_result = __map_result.get()
+        __process_run_successful = __map_result.successful()
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def map_by_args(self, function: Callable, args_iter: IterableType[IterableType] = (), chunksize: int = None) -> None:
+        __process_running_result = None
+
+        try:
+            __process_running_result = self._Thread_Pool.starmap(
+                func=function, iterable=args_iter, chunksize=chunksize)
+            __exception = None
+            __process_run_successful = False
+        except Exception as e:
+            __exception = e
+            __process_run_successful = False
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def async_map_by_args(self,
+                          function: Callable,
+                          args_iter: IterableType[IterableType] = (),
+                          chunksize: int = None,
+                          callback: Callable = None,
+                          error_callback: Callable = None) -> None:
+        __map_result = self._Thread_Pool.starmap_async(
+            func=function,
+            iterable=args_iter,
+            chunksize=chunksize,
+            callback=callback,
+            error_callback=error_callback)
+        __process_running_result = __map_result.get()
+        __process_run_successful = __map_result.successful()
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def imap_by_args(self, function: Callable, args_iter: IterableType = (), chunksize: int = 1) -> None:
+        __process_running_result = None
+
+        try:
+            imap_running_result = self._Thread_Pool.imap(func=function, iterable=args_iter, chunksize=chunksize)
+            __process_running_result = [result for result in imap_running_result]
+            __exception = None
+            __process_run_successful = False
+        except Exception as e:
+            __exception = e
+            __process_run_successful = False
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def imap_unordered_by_args(self, function: Callable, args_iter: IterableType = (), chunksize: int = 1) -> None:
+        __process_running_result = None
+
+        try:
+            imap_running_result = self._Thread_Pool.imap_unordered(func=function, iterable=args_iter, chunksize=chunksize)
+            __process_running_result = [result for result in imap_running_result]
+            __exception = None
+            __process_run_successful = False
+        except Exception as e:
+            __exception = e
+            __process_run_successful = False
+
+        # Save Running result state and Running result value as dict
+        self._result_saving(successful=__process_run_successful, result=__process_running_result)
+
+
+    def _result_saving(self, successful: bool, result: List):
+        process_result = {"successful": successful, "result": result}
+        # Saving value into list
+        self._Thread_Running_Result.append(process_result)
+
+
+    def close(self) -> None:
+        self._Thread_Pool.close()
+        self._Thread_Pool.join()
+
+
+    def terminal(self) -> None:
+        self._Thread_Pool.terminate()
+
+
+    def get_result(self) -> List[_ConcurrentResult]:
+        return self.result()
 
