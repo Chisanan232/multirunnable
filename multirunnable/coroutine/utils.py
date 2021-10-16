6a5126eec2c418eff63f97458f7ac2758b87e660
Chisanan232 <chi10211201@cycu.org.tw>
2021-10-16 14:14:22 +0800

    Fix the 'TypeError' issue about Python 3.10 removes parameter *loop* in sleep in package 'asyncio'.

--- multirunnable/coroutine/utils.py
+++ multirunnable/coroutine/utils.py
@@ -1,6 +1,9 @@
+from multirunnable import PYTHON_MAJOR_VERSION, PYTHON_MINOR_VERSION
+
 from abc import ABCMeta, abstractmethod
-import asyncio
-import gevent
+from gevent import sleep as gevent_sleep
+from asyncio import sleep as async_sleep, AbstractEventLoop, Future
+import logging
 
 
 
@@ -16,13 +19,20 @@ class GreenThreadWaiter(CoroutineWaiter):
 
     @staticmethod
     def sleep(seconds: int, ref: bool = True) -> None:
-        gevent.sleep(seconds=seconds, ref=ref)
+        gevent_sleep(seconds=seconds, ref=ref)
 
 
 
 class AsynchronousWaiter(CoroutineWaiter):
 
     @staticmethod
-    async def sleep(delay: float, result=None, loop: asyncio.AbstractEventLoop = None) -> asyncio.Future:
-        return await asyncio.sleep(delay=delay, result=result, loop=loop)
+    async def sleep(delay: float, result=None, loop: AbstractEventLoop = None) -> Future:
+        if PYTHON_MAJOR_VERSION == 3:
+            if PYTHON_MINOR_VERSION >= 10:
+                logging.info("Doesn't pass parameter 'loop' to asyncio.sleep.")
+                return await async_sleep(delay=delay, result=result)
+            return await async_sleep(delay=delay, result=result, loop=loop)
+        else:
+            from ..exceptions import VersionError
+            raise VersionError
 
