43bf7234410bb5bffad201dd4e60de70659153a5
Chisanan232 <chi10211201@cycu.org.tw>
2021-12-02 09:06:14 +0800

    Add persistence layer of database.

---
+++ multirunnable/persistence/database/layer.py
@@ -0,0 +1,40 @@
+from multirunnable.persistence.interface import DataPersistenceLayer
+from multirunnable.persistence.database.operator import DatabaseOperator
+
+from abc import ABCMeta, ABC, abstractmethod
+from typing import Tuple, TypeVar, Type, Generic, Any, Optional
+
+
+T = TypeVar("T")
+
+
+class DatabaseAccessObject(DataPersistenceLayer, ABC):
+
+    def __init__(self, **kwargs):
+        super(DatabaseAccessObject, self).__init__(**kwargs)
+
+
+
+class BaseDao(DatabaseAccessObject):
+
+    @property
+    @abstractmethod
+    def database_opt(self) -> DatabaseOperator:
+        pass
+
+
+    def execute(self, operator: Any, params: Tuple = None, multi: bool = False) -> Generic[T]:
+        return self.database_opt.execute(operator=operator, params=params, multi=multi)
+
+
+    def execute_many(self, operator: Any, seq_params: Tuple = None) -> Generic[T]:
+        return self.database_opt.execute_many(operator=operator, seq_params=seq_params)
+
+
+    def fetch_many(self, size: int = None) -> Generic[T]:
+        return self.database_opt.fetch_many(size=size)
+
+
+    def close(self) -> Generic[T]:
+        return self.database_opt.close()
+
