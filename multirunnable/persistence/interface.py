97e1875482e7f57c39f2333e9af8b6a0a1d28cab
Chisanan232 <chi10211201@cycu.org.tw>
2021-12-02 09:07:03 +0800

    Modify the interface from 'OceanDao' to 'DataPersistenceLayer'.

--- multirunnable/persistence/interface.py
+++ multirunnable/persistence/interface.py
@@ -2,13 +2,15 @@ from abc import ABCMeta, abstractmethod
 
 
 
-class OceanPersistence(metaclass=ABCMeta):
+class BasePersistence(metaclass=ABCMeta):
     pass
 
 
 
-class OceanDao(metaclass=ABCMeta):
-    pass
+class DataPersistenceLayer(metaclass=ABCMeta):
+
+    def __init__(self, **kwargs):
+        pass
 
 
 
