53dd84e55177a0185e2af4c1ba904a1adc0d4277
Chisanan232 <chi10211201@cycu.org.tw>
2021-12-07 22:03:06 +0800

    Remove unused exceptions.

--- multirunnable/persistence/exception.py
+++ multirunnable/persistence/exception.py
@@ -1,28 +1,4 @@
 """
-Database part exceptions
-"""
-
-
-class InvalidDriverException(Exception):
-
-    def __str__(self):
-        return "Driver object isn't StockMarketDriver."
-
-
-class InvalidHostTypeException(Exception):
-
-    def __str__(self):
-        return "Host type object isn't HostType."
-
-
-class PersistenceModeIsInvalid(Exception):
-
-    def __str__(self):
-        return "Mode is incorrect. " \
-               "Please use pyocean.persistence.mode.PersistenceMode."
-
-
-"""
 File part exceptions
 """
 
