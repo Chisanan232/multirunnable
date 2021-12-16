cb81cfd5dd666a33eebedd7e5287421e2fbcf52d
Chisanan232 <chi10211201@cycu.org.tw>
2021-12-16 17:52:11 +0800

    Add one new value 'Semaphore_Value'.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Dict
 
 
+# # # # Base APIs setting
 Worker_Size: int = 7
 Worker_Pool_Size: int = 7
 Task_Size: int = 7
@@ -11,3 +12,9 @@ Test_Function_Sleep_Time = 3
 Test_Function_Args: Tuple = (1, 2, "test_value")
 Test_Function_Kwargs: Dict = {"param_1": 1, "param_2": 2, "test_param": "test_value"}
 Test_Function_Multiple_Args = (Test_Function_Args, Test_Function_Args, Test_Function_Args)
+
+
+# # # # Lock APIs setting
+Semaphore_Value = 2
+
+
