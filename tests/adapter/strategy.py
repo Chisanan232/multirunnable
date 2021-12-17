dcf349e829400beb046b3e940a7154d36dd8d85a
Chisanan232 <chi10211201@cycu.org.tw>
2021-12-17 23:36:17 +0800

    Add testing items for module 'multirunnable.adapter.strategy'.

---
+++ tests/adapter/strategy.py
@@ -0,0 +1,96 @@
+from multirunnable.mode import RunningMode
+from multirunnable.adapter.strategy import StrategyAdapter, ExecutorStrategyAdapter, PoolStrategyAdapter
+from multirunnable.parallel.strategy import ProcessStrategy, ProcessPoolStrategy
+from multirunnable.concurrent.strategy import ThreadStrategy, ThreadPoolStrategy
+from multirunnable.coroutine.strategy import GreenThreadStrategy, GreenThreadPoolStrategy, AsynchronousStrategy
+
+from ..test_config import Worker_Size, Worker_Pool_Size, Task_Size
+
+import pytest
+
+
+_Worker_Size = Worker_Size
+_Worker_Pool_Size = Worker_Pool_Size
+_Task_Size = Task_Size
+
+
+# @pytest.fixture(scope="class")
+# def executor_strategy() -> ExecutorStrategyAdapter:
+#     return ExecutorStrategyAdapter(mode=, executors=)
+#
+#
+# @pytest.fixture(scope="class")
+# def pool_strategy() -> PoolStrategyAdapter:
+#     return PoolStrategyAdapter(mode=, pool_size=, tasks_size=)
+
+
+
+class TestAdapterStrategy:
+
+    @pytest.mark.skip(reason="Possible deprecated")
+    def test_get_simple(self):
+        pass
+
+
+    @pytest.mark.skip(reason="Possible deprecated")
+    def test_get_persistence(self):
+        pass
+
+
+
+class TestAdapterExecuteStrategy:
+
+    def test_get_simple_with_parallel(self):
+        _strategy_adapter = ExecutorStrategyAdapter(mode=RunningMode.Parallel, executors=_Worker_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, ProcessStrategy) is True, f"The type of strategy instance should be 'ProcessStrategy'."
+
+
+    def test_get_simple_with_concurrent(self):
+        _strategy_adapter = ExecutorStrategyAdapter(mode=RunningMode.Concurrent, executors=_Worker_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, ThreadStrategy) is True, f"The type of strategy instance should be 'ThreadStrategy'."
+
+
+    def test_get_simple_with_coroutine(self):
+        _strategy_adapter = ExecutorStrategyAdapter(mode=RunningMode.GreenThread, executors=_Worker_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, GreenThreadStrategy) is True, f"The type of strategy instance should be 'GreenThreadStrategy'."
+
+
+    def test_get_simple_with_asynchronous(self):
+        _strategy_adapter = ExecutorStrategyAdapter(mode=RunningMode.Asynchronous, executors=_Worker_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, AsynchronousStrategy) is True, f"The type of strategy instance should be 'AsynchronousStrategy'."
+
+
+    @pytest.mark.skip(reason="Possible deprecated")
+    def test_get_persistence(self):
+        pass
+
+
+
+class TestAdapterPoolStrategy:
+
+    def test_get_simple_with_parallel(self):
+        _strategy_adapter = PoolStrategyAdapter(mode=RunningMode.Parallel, pool_size=_Worker_Pool_Size, tasks_size=_Task_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, ProcessPoolStrategy) is True, f"The type of strategy instance should be 'ProcessPoolStrategy'."
+
+
+    def test_get_simple_with_concurrent(self):
+        _strategy_adapter = PoolStrategyAdapter(mode=RunningMode.Concurrent, pool_size=_Worker_Pool_Size, tasks_size=_Task_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, ThreadPoolStrategy) is True, f"The type of strategy instance should be 'ThreadPoolStrategy'."
+
+
+    def test_get_simple_with_coroutine(self):
+        _strategy_adapter = PoolStrategyAdapter(mode=RunningMode.GreenThread, pool_size=_Worker_Pool_Size, tasks_size=_Task_Size)
+        _strategy = _strategy_adapter.get_simple()
+        assert isinstance(_strategy, GreenThreadPoolStrategy) is True, f"The type of strategy instance should be 'GreenThreadPoolStrategy'."
+
+
+    @pytest.mark.skip(reason="Possible deprecated")
+    def test_get_persistence(self):
+        pass
+
