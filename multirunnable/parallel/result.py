9d4432e239d6f293efba91f5cd9bf9c70d4f83ff
Chisanan232 <chi10211201@cycu.org.tw>
2021-12-29 22:07:13 +0800

    Add 2 new properties in 'ParallelResult' and add 1 new object 'ProcessPoolResult'.

--- multirunnable/parallel/result.py
+++ multirunnable/parallel/result.py
@@ -1,7 +1,56 @@
 from multirunnable.framework.result import MRResult as _MRResult
 
+from typing import List, Any
+
 
 
 class ParallelResult(_MRResult):
-    pass
+
+    _PPID: str = ""
+    _Exit_Code: str = ""
+
+    @property
+    def ppid(self) -> str:
+        return self._PPID
+
+
+    @ppid.setter
+    def ppid(self, ppid: str) -> None:
+        self._PPID = ppid
+
+
+    @property
+    def exit_code(self) -> str:
+        return self._Exit_Code
+
+
+    @exit_code.setter
+    def exit_code(self, exit_code: str) -> None:
+        self._Exit_Code = exit_code
+
+
+
+class ProcessPoolResult:
+
+    _Data: List[Any] = None
+    _Is_Successful: bool = None
+
+    @property
+    def data(self) -> List[Any]:
+        return self._Data
+
+
+    @data.setter
+    def data(self, d) -> None:
+        self._Data = d
+
+
+    @property
+    def is_successful(self) -> bool:
+        return self._Is_Successful
+
+
+    @is_successful.setter
+    def is_successful(self, successful) -> None:
+        self._Is_Successful = successful
 
