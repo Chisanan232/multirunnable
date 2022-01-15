9f283d97e32d97e241dc93ee6fb2ef2bf5cbd6c9
Chisanan232 <chi10211201@cycu.org.tw>
2022-01-15 16:40:52 +0800

    Update setup.py setting.

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multirunnable",
-    version="0.15.2",
+    version="0.16.0",
     author="Liu, Bryant",
     author_email="chi10211201@cycu.org.tw",
+    url="https://multirunnable.readthedocs.io",
+    license="Apache License 2.0",
     description="A Python framework integrates running multiple tasks simultaneously with different strategy.",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -16,7 +18,7 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        # "Topic :: Build MultiRunnable Programming",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
@@ -31,9 +33,6 @@ setuptools.setup(
     install_requires=[
         "gevent",
         "multipledispatch==0.6.0",
-        "colorama==0.4.1",
-        "kafka-python==2.0.2",
-        "openpyxl==2.6.1",
-        "deprecated==1.2.11"
+        "openpyxl==2.6.1"
     ]
 )
