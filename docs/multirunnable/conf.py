e6c6f666e5a6291a96e9dbeae9a45d091252a121
Chisanan232 <chi10211201@cycu.org.tw>
2022-01-15 12:05:24 +0800

    Add extendsion of documentation tool - sphinx. [skip ci]

--- docs/multirunnable/conf.py
+++ docs/multirunnable/conf.py
@@ -35,6 +35,7 @@ release = __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
