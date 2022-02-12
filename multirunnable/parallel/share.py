556cf49bef75b4d42c6e5791187cc98a5eb306d5
Chisanan232 <chi10211201@cycu.org.tw>
2022-02-12 23:48:06 +0800

    Comment out the unused code.

--- multirunnable/parallel/share.py
+++ multirunnable/parallel/share.py
@@ -13,18 +13,18 @@ Note:
     * Python Bug record: https://bugs.python.org/issue30256
 """
 
-# Backup original AutoProxy function
-backup_autoproxy = managers.AutoProxy
-
-
-# Defining a new AutoProxy that handles unwanted key argument 'manager_owned'
-def redefined_autoproxy(token, serializer, manager=None, authkey=None, exposed=None, incref=True, manager_owned=True):
-    # Calling original AutoProxy without the unwanted key argument
-    return backup_autoproxy(token, serializer, manager, authkey, exposed, incref)
-
-
-# Updating AutoProxy definition in multiprocessing.managers package
-managers.AutoProxy = redefined_autoproxy
+# # Backup original AutoProxy function
+# backup_autoproxy = managers.AutoProxy
+#
+#
+# # Defining a new AutoProxy that handles unwanted key argument 'manager_owned'
+# def redefined_autoproxy(token, serializer, manager=None, authkey=None, exposed=None, incref=True, manager_owned=True):
+#     # Calling original AutoProxy without the unwanted key argument
+#     return backup_autoproxy(token, serializer, manager, authkey, exposed, incref)
+#
+#
+# # Updating AutoProxy definition in multiprocessing.managers package
+# managers.AutoProxy = redefined_autoproxy
 
 
 _Assign_Manager_Flag: Dict[str, bool] = {}
