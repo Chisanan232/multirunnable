0932f3d212ce89c31d5bf970e4bcfc9da192026d
Chisanan232 <chi10211201@cycu.org.tw>
2022-02-16 21:23:13 +0800

    Add new modules definition 'context'.

--- multirunnable/framework/runnable/__init__.py
+++ multirunnable/framework/runnable/__init__.py
@@ -1,3 +1,4 @@
+from .context import BaseContext
 from .strategy import GeneralRunnableStrategy, PoolRunnableStrategy, AsyncRunnableStrategy, Resultable
 from .result import BaseResult, MRResult, PoolResult, ResultState
 from .synchronization import PosixThreadLock, PosixThreadCommunication
---
+++ multirunnable/framework/runnable/context.py
@@ -0,0 +1,55 @@
+from abc import ABCMeta, abstractmethod
+from typing import List, Any
+
+
+
+class BaseContext(metaclass=ABCMeta):
+
+    @staticmethod
+    @abstractmethod
+    def get_current_worker() -> Any:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def get_parent_worker() -> Any:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def current_worker_is_parent() -> bool:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def get_current_worker_ident() -> str:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def get_current_worker_name() -> str:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def current_worker_is_alive() -> bool:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def active_workers_count() -> List[Any]:
+        pass
+
+
+    @staticmethod
+    @abstractmethod
+    def children_workers() -> List[Any]:
+        pass
+
+
