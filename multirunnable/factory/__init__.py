40209dddf7f8c777fca0421aa5792001c72d9d60
Chisanan232 <chi10211201@cycu.org.tw>
2022-02-18 07:11:46 +0800

    Remove deprecated class and its importing.

--- multirunnable/factory/__init__.py
+++ multirunnable/factory/__init__.py
@@ -2,4 +2,4 @@ from multirunnable.factory.queue import Queue
 from multirunnable.factory.lock import LockFactory, RLockFactory, SemaphoreFactory, BoundedSemaphoreFactory
 from multirunnable.factory.communication import EventFactory, ConditionFactory
 from multirunnable.factory.collection import FeatureList, QueueTaskList
-from multirunnable.factory.strategy import StrategyAdapter
+from multirunnable.factory.strategy import ExecutorStrategyAdapter, PoolStrategyAdapter
--- multirunnable/factory/strategy.py
+++ multirunnable/factory/strategy.py
@@ -1,13 +1,11 @@
 from multirunnable.framework.runnable.strategy import (
-    RunnableStrategy as _RunnableStrategy,
     GeneralRunnableStrategy as _GeneralRunnableStrategy,
-    PoolRunnableStrategy as _PoolRunnableStrategy,
-    AsyncRunnableStrategy as _AsyncRunnableStrategy)
+    PoolRunnableStrategy as _PoolRunnableStrategy)
 from multirunnable.mode import RunningMode as _RunningMode
 from multirunnable._import_utils import ImportMultiRunnable as _ImportMultiRunnable
 
 from abc import ABCMeta
-from typing import Dict, Union
+from typing import Dict
 
 
 
@@ -16,22 +14,6 @@ class BaseStrategyAdapter(metaclass=ABCMeta):
     def __init__(self, mode: _RunningMode):
         self._running_info: Dict[str, str] = mode.value
         self._module: str = self._running_info.get("strategy_module")
-
-
-
-class StrategyAdapter(BaseStrategyAdapter):
-
-    def __init__(self, mode: _RunningMode, worker_num: int):
-        super().__init__(mode=mode)
-        self._process_num = worker_num
-        self.__strategy_cls_name: str = self._running_info.get("strategy")
-
-
-    def get_simple_strategy(self) -> Union[_RunnableStrategy, _AsyncRunnableStrategy]:
-        __strategy_cls = _ImportMultiRunnable.get_class(pkg_path=self._module, cls_name=self.__strategy_cls_name)
-        __strategy_instance = __strategy_cls(workers_num=self._process_num)
-        # __strategy_instance = cast(Union[RunnableStrategy, AsyncRunnableStrategy], __strategy_instance)
-        return __strategy_instance
 
 
 
