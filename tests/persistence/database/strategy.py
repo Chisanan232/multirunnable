96db36f0574e99beba9e46b1a4abaaf32f577f9c
Chisanan232 <chi10211201@cycu.org.tw>
2022-02-19 18:37:26 +0800

    Finish implementations of the skipped testing items.

--- tests/persistence/database/strategy.py
+++ tests/persistence/database/strategy.py
@@ -1,9 +1,14 @@
-from multirunnable.persistence.database.strategy import get_connection_pool
+from multirunnable.persistence.database.strategy import get_connection_pool, database_connection_pools, Globalize
+from multirunnable.exceptions import GlobalizeObjectError
 
-from ...test_config import Test_Pool_Name, Test_Pool_Size,Database_Config, Database_Pool_Config
+from ...test_config import (
+    Test_Pool_Name, Test_Pool_Size,Database_Config, Database_Pool_Config,
+    SELECT_TEST_DATA_SQL, INSERT_TEST_DATA_SQL, DELETE_TEST_DATA_SQL
+)
 from ._test_db_implement import MySQLSingleConnection, MySQLDriverConnectionPool
 
 import pytest
+import re
 
 
 
@@ -21,11 +26,36 @@ def connection_pool_strategy():
     return MySQLDriverConnectionPool(**Database_Pool_Config)
 
 
+def _select_data(_cursor) -> list:
+    _cursor.execute(SELECT_TEST_DATA_SQL)
+    _data = _cursor.fetchall()
+    return _data
+
+
+def _close_instance(_conn, _cursor):
+    _cursor.close()
+    _conn.close()
+
+
 class TestPersistenceDatabaseOneConnection:
 
+    def test_repr(self, single_connection_strategy: MySQLSingleConnection):
+        _strategy_inst_repr = repr(single_connection_strategy)
+        _chksum = re.search(
+            re.escape(single_connection_strategy.__class__.__name__) +
+            "\(" + re.escape(str(single_connection_strategy.database_config)) + "\)" ,
+            str(_strategy_inst_repr))
+
+        assert _chksum is not None, "The __repr__ info should be map to the format."
+
+
     def test_connect_database(self, single_connection_strategy: MySQLSingleConnection):
-        _connection = single_connection_strategy.connection
+        _connection = single_connection_strategy.current_connection
         assert _connection is not None, "It should return a database connection instance."
+        assert _connection.is_connected() is True, "The connection instance should be connected."
+
+        _get_connection = single_connection_strategy.get_one_connection()
+        assert _get_connection is _connection is not None, "These values should be the same."
 
 
     def test_database_config(self, single_connection_strategy: MySQLSingleConnection):
@@ -55,13 +85,58 @@ class TestPersistenceDatabaseOneConnection:
         assert _connection is not None, "It should return a database connection instance."
 
 
-    @pytest.mark.skip(reason="Consider this feature testing logic.")
     def test_commit(self, single_connection_strategy: MySQLSingleConnection):
-        single_connection_strategy.commit()
+        # # Insert data but doesn't commit it and close cursor and connection.
+        _connection = single_connection_strategy.connect_database()
+        _connection = single_connection_strategy.current_connection
+        _cursor = _connection.cursor()
+
+        TestPersistenceDatabaseOneConnection._insert_data(cursor=_cursor, commit=False)
+        _close_instance(_conn=_connection, _cursor=_cursor)
+
+        # # Select data to check. Insert data and commit it.
+        _new_connection = single_connection_strategy.reconnect()
+        _new_cursor = _new_connection.cursor()
+
+        _data = _select_data(_cursor=_new_cursor)
+        assert _data == [], f"It should get an empty dataset right now."
+        TestPersistenceDatabaseOneConnection._insert_data(cursor=_new_cursor, commit=True, connection_strategy=single_connection_strategy)
+        _close_instance(_conn=_new_connection, _cursor=_new_cursor)
+
+        # # Select data to check. Delete testing data and commit it.
+        _latest_connection = single_connection_strategy.reconnect()
+        _latest_cursor = _latest_connection.cursor()
+
+        _data = _select_data(_cursor=_latest_cursor)
+        assert _data != [] and len(_data) == 1, f"It should get an empty dataset right now."
+        TestPersistenceDatabaseOneConnection._delete_data(cursor=_latest_cursor, connection_strategy=single_connection_strategy)
+        _close_instance(_conn=_latest_connection, _cursor=_latest_cursor)
 
 
-    @pytest.mark.skip(reason="Consider this feature testing logic.")
+    @staticmethod
+    def _delete_data(cursor, connection_strategy) -> None:
+        cursor.execute(DELETE_TEST_DATA_SQL)
+        connection_strategy.commit()
+        cursor.execute(SELECT_TEST_DATA_SQL)
+        _data = cursor.fetchall()
+        assert _data == [], ""
+
+
+    @staticmethod
+    def _insert_data(cursor, commit: bool, connection_strategy=None) -> None:
+        cursor.execute(INSERT_TEST_DATA_SQL)
+
+        if commit is True:
+            if connection_strategy is None:
+                raise ValueError("ConnectionStrategy cannot be empty if option *commit* is True.")
+            connection_strategy.commit()
+
+
     def test_close(self, single_connection_strategy: MySQLSingleConnection):
+        _connection = single_connection_strategy.reconnect()
+        _is_connected = _connection.is_connected()
+        assert _is_connected is True, "The database connection instance should be connected."
+
         try:
             single_connection_strategy.close()
         except Exception as e:
@@ -69,15 +144,35 @@ class TestPersistenceDatabaseOneConnection:
         else:
             assert True, "It closes the database connection and cursor instances normally."
 
+        _connection = single_connection_strategy.current_connection
+        assert _connection is not None, "The database connection instance should not be None object."
+        _is_connected = _connection.is_connected()
+        assert _is_connected is False, "The database connection instance should be closed."
+
 
 
 class TestPersistenceDatabaseConnectionPool:
+
+    def test_repr(self, connection_pool_strategy: MySQLDriverConnectionPool):
+        _strategy_inst_repr = repr(connection_pool_strategy)
+        _chksum = re.search(
+            re.escape(connection_pool_strategy.__class__.__name__) +
+            "\(" + re.escape(str(connection_pool_strategy.database_config)) + "\)" ,
+            str(_strategy_inst_repr))
+
+        assert _chksum is not None, "The __repr__ info should be map to the format."
+
 
     def test_connect_database(self, connection_pool_strategy: MySQLDriverConnectionPool):
         _conn_pool = get_connection_pool(pool_name=Test_Pool_Name)
         assert _conn_pool is not None, f"It should return a connection pool object back with the pool name {Test_Pool_Name}."
         assert _conn_pool.pool_name == Test_Pool_Name, f"The pool name we get from the connection pool instance should be equal to the name we set '{Test_Pool_Name}' before."
         assert int(_conn_pool.pool_size) == int(Test_Pool_Size), f"The pool size we get from the connection pool instance should be equal to the number we set '{Test_Pool_Size}' before."
+
+        _all_pools = database_connection_pools()
+        assert len(_all_pools) == 1, "The length should be '1' because we only instantiate one database connection pool object."
+        assert Test_Pool_Name in _all_pools.keys(), f"The pool name '{Test_Pool_Name} should be in the keys of {_all_pools}.'"
+        assert _all_pools[Test_Pool_Name] is _conn_pool, f"The instances '{_all_pools[Test_Pool_Name]}' and '{_conn_pool}' should be the same."
 
 
     def test_database_config(self, connection_pool_strategy: MySQLDriverConnectionPool):
@@ -148,22 +243,78 @@ class TestPersistenceDatabaseConnectionPool:
 
         _conn = connection_pool_strategy.get_one_connection(pool_name=Test_Pool_Name)
         assert _conn is not None, f"It should get a connection instance from the pool with pool name '{Test_Pool_Name}'."
+        # # Release connection instance back to pool
+        _conn.close()
 
 
-    @pytest.mark.skip(reason="Consider this feature testing logic.")
     def test_commit(self, connection_pool_strategy: MySQLDriverConnectionPool):
-        connection_pool_strategy.commit()
+        # # Insert data but doesn't commit it and close cursor and connection.
+        _connection = connection_pool_strategy.get_one_connection(pool_name=Test_Pool_Name)
+        _cursor = _connection.cursor()
+
+        TestPersistenceDatabaseConnectionPool._insert_data(conn=_connection, cursor=_cursor, commit=False)
+        _close_instance(_conn=_connection, _cursor=_cursor)
+
+        # # Select data to check. Insert data and commit it.
+        _new_connection = connection_pool_strategy.reconnect()
+        _new_cursor = _new_connection.cursor()
+
+        _data = _select_data(_cursor=_new_cursor)
+        assert _data == [], f"It should get an empty dataset right now."
+        TestPersistenceDatabaseConnectionPool._insert_data(conn=_new_connection, cursor=_new_cursor, commit=True, connection_strategy=connection_pool_strategy)
+        _close_instance(_conn=_new_connection, _cursor=_new_cursor)
+
+        # # Select data to check. Delete testing data and commit it.
+        _latest_connection = connection_pool_strategy.reconnect()
+        _latest_cursor = _latest_connection.cursor()
+
+        _data = _select_data(_cursor=_latest_cursor)
+        assert _data != [] and len(_data) == 1, f"It should get an empty dataset right now."
+        TestPersistenceDatabaseConnectionPool._delete_data(conn=_latest_connection, cursor=_latest_cursor, connection_strategy=connection_pool_strategy)
+        _close_instance(_conn=_latest_connection, _cursor=_latest_cursor)
 
 
-    @pytest.mark.skip(reason="Consider this feature testing logic.")
+    @staticmethod
+    def _delete_data(conn, cursor, connection_strategy) -> None:
+        cursor.execute(DELETE_TEST_DATA_SQL)
+        connection_strategy.commit(conn=conn)
+        cursor.execute(SELECT_TEST_DATA_SQL)
+        _data = cursor.fetchall()
+        assert _data == [], ""
+
+
+    @staticmethod
+    def _insert_data(conn, cursor, commit: bool, connection_strategy=None) -> None:
+        cursor.execute(INSERT_TEST_DATA_SQL)
+
+        if commit is True:
+            if connection_strategy is None:
+                raise ValueError("ConnectionStrategy cannot be empty if option *commit* is True.")
+            connection_strategy.commit(conn=conn)
+
+
     def test_close(self, connection_pool_strategy: MySQLDriverConnectionPool):
         _conn = connection_pool_strategy.get_one_connection(pool_name=Test_Pool_Name)
-        connection_pool_strategy.current_pool_name = Test_Pool_Name
-        connection_pool_strategy.close()
+        _cursor = _conn.cursor()
+        assert _cursor is not None, "It should get the cursor instance without any issue."
+
+        _cursor.close()
+        connection_pool_strategy.close_connection(conn=_conn)
+
+        try:
+            _cursor = _conn.cursor()
+        except AttributeError as ae:
+            assert "'NoneType' object has no attribute 'cursor'" in str(ae), f""
 
 
-    @pytest.mark.skip(reason="Consider this feature testing logic.")
-    def test_close_pool(self, connection_pool_strategy: MySQLDriverConnectionPool):
-        # connection_pool_strategy.current_pool_name = Test_Pool_Name
-        connection_pool_strategy.close_pool()
+
+class TestGlobalize:
+
+    def test_connection_pool_with_invalid_argument(self):
+        try:
+            Globalize.connection_pool(name="test", pool=None)
+        except GlobalizeObjectError as ge:
+            assert str(ge), ""
+        else:
+            assert False, "It should raise an exception about target object of option *pool* cannot be a None type object."
 
