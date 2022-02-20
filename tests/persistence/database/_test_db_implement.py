7ff5697e0b0e829d2f9879d8a4ef77558194b813
Chisanan232 <chi10211201@cycu.org.tw>
2022-02-20 12:00:05 +0800

    Modify the implementations of modules in 'persistence.database' for testing.

--- tests/persistence/database/_test_db_implement.py
+++ tests/persistence/database/_test_db_implement.py
@@ -1,4 +1,4 @@
-from multirunnable.persistence.database.strategy import get_connection_pool, database_connection_pools, BaseDatabaseConnection, BaseSingleConnection, BaseConnectionPool
+from multirunnable.persistence.database.strategy import get_connection_pool, BaseDatabaseConnection, BaseSingleConnection, BaseConnectionPool
 from multirunnable.persistence.database.operator import DatabaseOperator
 from multirunnable.parallel.share import sharing_in_processes
 
@@ -10,7 +10,6 @@ from mysql.connector.errors import PoolError
 from mysql.connector.cursor import MySQLCursor
 import mysql.connector
 import time
-import os
 
 
 
@@ -27,37 +26,24 @@ class MySQLDriverConnectionPoolProxy(NamespaceProxy):
 # @sharing_in_processes()
 class MySQLSingleConnection(BaseSingleConnection):
 
-    @property
-    def connection(self) -> MySQLConnection:
-        """
-        Note:
-            For resolving this issue, we should do something to avoid this issue.
-            However, it has exception about "TypeError: can't pickle _mysql_connector.MySQL objects" for database package.
-        :return:
-        """
-        return self._database_connection
-
-
     def connect_database(self, **kwargs) -> MySQLConnection:
-        # return mysql.connector.connect(**self._Database_Config)
         _connection = mysql.connector.connect(**kwargs)
         return _connection
 
 
     def commit(self) -> None:
-        self.connection.commit()
+        self.current_connection.commit()
 
 
-    def close(self) -> None:
-        if self.connection is not None and self.connection.is_connected():
-            self.connection.close()
+    def close_connection(self) -> None:
+        if self.current_connection is not None and self.current_connection.is_connected() is True:
+            self.current_connection.close()
 
 
 # @sharing_in_processes
 class MySQLDriverConnectionPool(BaseConnectionPool):
 
     def connect_database(self, **kwargs) -> MySQLConnectionPool:
-        # connection_pool = MySQLConnectionPool(**self._Database_Config)
         connection_pool = MySQLConnectionPool(**kwargs)
         return connection_pool
 
@@ -65,32 +51,30 @@ class MySQLDriverConnectionPool(BaseConnectionPool):
     def get_one_connection(self, pool_name: str = "", **kwargs) -> PooledMySQLConnection:
         while True:
             try:
-                # return self.database_connection_pool.get_connection()
                 __connection = get_connection_pool(pool_name=pool_name).get_connection()
                 return __connection
             except PoolError as e:
                 time.sleep(5)
+                raise e
             except AttributeError as ae:
-                raise ConnectionError(f"Cannot get the one connection instance from connection pool because it doesn't exist the connection pool with the name '{pool_name}'.")
+                if "'NoneType' object has no attribute 'get_connection'" in str(ae):
+                    raise ConnectionError(f"Cannot get the one connection instance from connection pool because it doesn't exist the connection pool with the name '{pool_name}'.")
+                else:
+                    raise ae
 
 
-    def commit(self) -> None:
-        self.connection.commit()
+    def commit(self, conn: PooledMySQLConnection) -> None:
+        conn.commit()
 
 
-    def close_pool(self) -> None:
+    def close_connection(self, conn: PooledMySQLConnection) -> None:
+        if conn is not None:
+            conn.close()
+
+
+    def close_pool(self, pool) -> None:
         # self.get_connection_pool(pool_name=pool_name).close()
         pass
-
-
-    def close(self) -> None:
-        pass
-        # if conn.is_connected():
-        #     cursor.close()
-        #     conn.close()
-        #     print(f"MySQL connection is closed. - PID: {os.getpid()}")
-        # else:
-        #     print("Connection has been disconnect or be killed before.")
 
 
 # @sharing_in_processes()
@@ -126,22 +110,22 @@ class MySQLOperator(DatabaseOperator):
         return self._cursor.executemany(operation=operator, seq_params=seq_params)
 
 
-    def fetch(self) -> MySQLCursor:
-        return self._cursor.fetch()
-
-
-    def fetch_one(self) -> MySQLCursor:
+    def fetch_one(self) -> list:
         return self._cursor.fetchone()
 
 
-    def fetch_many(self, size: int = None) -> MySQLCursor:
+    def fetch_many(self, size: int = None) -> list:
         return self._cursor.fetchmany(size=size)
 
 
-    def fetch_all(self) -> MySQLCursor:
+    def fetch_all(self) -> list:
         return self._cursor.fetchall()
 
 
     def reset(self) -> None:
         self._cursor.reset()
+
+
+    def close_cursor(self) -> None:
+        self._cursor.close()
 
