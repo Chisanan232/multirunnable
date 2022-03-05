67096a3a7ad1249e4c48a4893d41089fa6629900
Chisanan232 <chi10211201@cycu.org.tw>
2022-03-05 23:31:21 +0800

    Implement all skipped testing items and add some new testing cases for module '.saver'.

--- tests/persistence/file/saver.py
+++ tests/persistence/file/saver.py
@@ -1,20 +1,31 @@
-from collections import namedtuple
-
-from multirunnable.persistence.file.saver import FileSaver, ArchiverSaver
+from multirunnable import set_mode, get_current_mode, RunningMode
+from multirunnable.parallel.strategy import Global_Manager
+from multirunnable.persistence.file import SavingStrategy, SavingMediator
+from multirunnable.persistence.file.saver import FileSaver, ArchiverSaver, _Done_Flag, _Do_Nothing_Flag
 from multirunnable.persistence.file.files import File
 from multirunnable.persistence.file.archivers import Archiver
 
-from .._data import Test_Data_List, Test_JSON_Data
+from ...test_config import Under_Test_RunningModes
+from .._data import Test_Data_List
 
+from collections import namedtuple
 from typing import List
+import multiprocessing
+import threading
 import pytest
+import os
 
+
+Global_Run_Flags: dict = Global_Manager.dict({
+    "Run_Open_Process_Flag": False,
+    "Run_Write_Process_Flag": False,
+    "Run_Close_Process_Flag": False
+})
 
 Run_Open_Process_Flag: bool = False
 Run_Write_Process_Flag: bool = False
 Run_Close_Process_Flag: bool = False
-Run_Procedure_List: List[str] = []
-Run_Result_Data_List = []
+Run_Procedure_List: List[str] = Global_Manager.list()
 
 Compress_Initial_Flag: bool = False
 Compress_Process_Flag: bool = False
@@ -25,22 +36,22 @@ Compress_Result_Data_List = []
 class _ExampleTestingFile(File):
 
     def open(self) -> None:
-        global Run_Open_Process_Flag, Run_Procedure_List
-        Run_Open_Process_Flag = True
+        global Global_Run_Flags, Run_Procedure_List
+        Global_Run_Flags["Run_Open_Process_Flag"] = True
         Run_Procedure_List.append("open")
         print("Running 'open' process to initial file object.")
 
 
     def write(self, data: List[list]) -> None:
-        global Run_Write_Process_Flag, Run_Procedure_List
-        Run_Write_Process_Flag = True
+        global Global_Run_Flags, Run_Procedure_List
+        Global_Run_Flags["Run_Write_Process_Flag"] = True
         Run_Procedure_List.append("write")
         print("Running 'write' process to save data into target file object.")
 
 
     def close(self) -> None:
-        global Run_Close_Process_Flag, Run_Procedure_List
-        Run_Close_Process_Flag = True
+        global Global_Run_Flags, Run_Procedure_List
+        Global_Run_Flags["Run_Close_Process_Flag"] = True
         Run_Procedure_List.append("close")
         print("Running 'close' process to finish file object.")
 
@@ -49,11 +60,42 @@ class _ExampleTestingFile(File):
         pass
 
 
+class _TestProcess(multiprocessing.Process):
+
+    def __init__(self, fs):
+        super().__init__()
+        self.fs = fs
+
+    def run(self) -> None:
+        _thread_result = self.fs.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+        assert _thread_result == _Done_Flag, ""
+
+
+class _TestThread(threading.Thread):
+
+    def __init__(self, fs):
+        super().__init__()
+        self.fs = fs
+
+    def run(self) -> None:
+        _thread_result = self.fs.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+        assert _thread_result == _Done_Flag, ""
+
+
+@pytest.fixture(scope="function")
+def file_saver(request) -> FileSaver:
+    set_mode(mode=request.param)
+
+    TestFileSaver._init_flag()
+
+    fs = FileSaver(file=_ExampleTestingFile())
+    return fs
+
+
 
 class TestFileSaver:
 
     def test_file_saving_procedure(self):
-
         class TestErrorFormatter:
             pass
 
@@ -70,31 +112,116 @@ class TestFileSaver:
         # Test for CSV
         fs = FileSaver(file=_ExampleTestingFile())
         fs.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
-        assert Run_Open_Process_Flag is True, "It should experience 'open' process."
-        assert Run_Write_Process_Flag is True, "It should experience 'write' process."
-        assert Run_Close_Process_Flag is True, "It should experience 'close' process."
+        global Global_Run_Flags, Run_Procedure_List
+        assert Global_Run_Flags["Run_Open_Process_Flag"] is True, "It should experience 'open' process."
+        assert Global_Run_Flags["Run_Write_Process_Flag"] is True, "It should experience 'write' process."
+        assert Global_Run_Flags["Run_Close_Process_Flag"] is True, "It should experience 'close' process."
         assert Run_Procedure_List[0] == "open", "It should run 'open' process first."
         assert Run_Procedure_List[1] == "write", "It should run 'write' process second."
         assert Run_Procedure_List[2] == "close", "It should run 'close' process finally."
 
 
+    @pytest.mark.parametrize(
+        argnames="file_saver",
+        argvalues=Under_Test_RunningModes,
+        indirect=True
+    )
+    def test_saving_with_one_worker_one_file(self, file_saver: FileSaver):
+        _mediator = SavingMediator()
+        file_saver.register(mediator=_mediator, strategy=SavingStrategy.ONE_THREAD_ONE_FILE)
+
+        if get_current_mode(force=True) is RunningMode.Parallel:
+            _worker = _TestProcess(file_saver)
+        else:
+            _worker = _TestThread(file_saver)
+        _worker.start()
+        _worker.join()
+
+        _result = file_saver.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+        assert _result == _Do_Nothing_Flag, ""
+
+        global Global_Run_Flags, Run_Procedure_List
+        assert Global_Run_Flags["Run_Open_Process_Flag"] is True, "It should experience 'open' process."
+        assert Global_Run_Flags["Run_Write_Process_Flag"] is True, "It should experience 'write' process."
+        assert Global_Run_Flags["Run_Close_Process_Flag"] is True, "It should experience 'close' process."
+        assert Run_Procedure_List[0] == "open", "It should run 'open' process first."
+        assert Run_Procedure_List[1] == "write", "It should run 'write' process second."
+        assert Run_Procedure_List[2] == "close", "It should run 'close' process finally."
+
+
+    @pytest.mark.parametrize(
+        argnames="file_saver",
+        argvalues=Under_Test_RunningModes,
+        indirect=True
+    )
+    def test_saving_with_all_workers_one_file(self, file_saver: FileSaver):
+
+        class _TestThread(threading.Thread):
+            def run(self) -> None:
+                _thread_result = file_saver.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+                assert _thread_result is not None, ""
+
+        _mediator = SavingMediator()
+        file_saver.register(mediator=_mediator, strategy=SavingStrategy.ALL_THREADS_ONE_FILE)
+
+        _thread = _TestThread()
+        _thread.start()
+        _thread.join()
+
+        _result = file_saver.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+        assert _result == _Done_Flag, ""
+
+        global Global_Run_Flags, Run_Procedure_List
+        assert Global_Run_Flags["Run_Open_Process_Flag"] is True, "It should experience 'open' process."
+        assert Global_Run_Flags["Run_Write_Process_Flag"] is True, "It should experience 'write' process."
+        assert Global_Run_Flags["Run_Close_Process_Flag"] is True, "It should experience 'close' process."
+        assert Run_Procedure_List[0] == "open", "It should run 'open' process first."
+        assert Run_Procedure_List[1] == "write", "It should run 'write' process second."
+        assert Run_Procedure_List[2] == "close", "It should run 'close' process finally."
+
+
+    @pytest.mark.parametrize(
+        argnames="file_saver",
+        argvalues=Under_Test_RunningModes,
+        indirect=True
+    )
+    def test_saving_with_one_worker_one_file_and_compress_all(self, file_saver: FileSaver):
+
+        class _TestThread(threading.Thread):
+            def run(self) -> None:
+                _thread_result = file_saver.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+                assert _thread_result is not None, ""
+
+        _mediator = SavingMediator()
+        file_saver.register(mediator=_mediator, strategy=SavingStrategy.ONE_THREAD_ONE_FILE_AND_COMPRESS_ALL)
+
+        _thread = _TestThread()
+        _thread.start()
+        _thread.join()
+
+        _result = file_saver.save(file="no.no", mode="yee", encoding="UTF-8", data=Test_Data_List)
+        assert _result == _Do_Nothing_Flag, ""
+
+        assert Run_Open_Process_Flag is False, "It should not experience 'open' process."
+        assert Run_Write_Process_Flag is False, "It should not experience 'write' process."
+        assert Run_Close_Process_Flag is False, "It should not experience 'close' process."
+        assert len(Run_Procedure_List) == 0, "It should have nothing in running procedure list."
+
+
     @staticmethod
     def _init_flag():
-        global Run_Open_Process_Flag, Run_Write_Process_Flag, Run_Close_Process_Flag, Run_Result_Data_List
-        Run_Open_Process_Flag = False
-        Run_Write_Process_Flag = False
-        Run_Close_Process_Flag = False
-        Run_Result_Data_List = []
+        global Global_Run_Flags, Run_Procedure_List
+        Global_Run_Flags["Run_Open_Process_Flag"] = False
+        Global_Run_Flags["Run_Write_Process_Flag"] = False
+        Global_Run_Flags["Run_Close_Process_Flag"] = False
+        Run_Procedure_List[:] = []
 
 
-    @pytest.mark.skip(reason="Not implement testing logic.")
-    def test_archiver_saver_procedure(self):
-        pass
-
-
-    @pytest.mark.skip(reason="Not implement testing logic.")
-    def test_saving_stream_procedure(self):
-        pass
+    @staticmethod
+    def _check_file_and_remove_it(_file_path: str):
+        _exist_file = os.path.exists(_file_path)
+        assert _exist_file is True, f"It should exist file {_file_path}."
+        os.remove(_file_path)
 
 
 
@@ -121,7 +248,7 @@ class _ExampleTestingArchiver(Archiver):
 
 class TestArchiverSaver:
 
-    def test_file_saving_procedure(self):
+    def test_archiver_saving_procedure(self):
 
         class TestErrorFormatter:
             pass
@@ -153,12 +280,6 @@ class TestArchiverSaver:
         Compress_Initial_Flag = False
         Compress_Initial_Flag = False
         Compress_Close_Flag = False
-        Compress_Result_Data_List = []
-
-
-    @pytest.mark.skip(reason="Not implement testing logic.")
-    def test_archiver_saver_procedure(self):
-        pass
-
+        Compress_Result_Data_List[:] = []
 
 
