ae0f415f735a42808b5a51d235cd663626e18870
Chisanan232 <chi10211201@cycu.org.tw>
2022-03-15 11:47:29 +0800

    Remove unused code which be commented out.

--- multirunnable/parallel/queue.py
+++ multirunnable/parallel/queue.py
@@ -1,20 +1,6 @@
-# from multirunnable.framework.runnable.queue import BaseQueueType as _BaseQueueType
-
-# from multiprocessing import (
-#     Queue as Process_Queue,
-#     SimpleQueue as Process_SimpleQueue,
-#     JoinableQueue as Process_JoinableQueue)
-# from typing import Union
-
+from typing import Union
 from multiprocessing import Queue, SimpleQueue, JoinableQueue
 
 
-# ProcessQueueDataType = Union[Process_Queue, Process_SimpleQueue, Process_JoinableQueue]
-
-
-# class ProcessQueueType(_BaseQueueType):
-#
-#     Queue = Process_Queue()
-#     SimpleQueue = Process_SimpleQueue()
-#     JoinableQueue = Process_JoinableQueue()
+ProcessQueueDataType = Union[Queue, SimpleQueue, JoinableQueue]
 
