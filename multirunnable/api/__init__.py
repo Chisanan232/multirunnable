e5cfc7daf546714a9016bbfa26a5e786e143ce38
Chisanan232 <chi10211201@cycu.org.tw>
2022-03-17 06:34:17 +0800

    Modify the import.

--- multirunnable/api/__init__.py
+++ multirunnable/api/__init__.py
@@ -5,69 +5,19 @@ from multirunnable.api.decorator import (
     RunWith as _RunWith,
     AsyncRunWith as _AsyncRunWith)
 from multirunnable.api.operator import (
-    LockAdapterOperator as __LockOperator,
-    RLockOperator as _RLockOperator,
-    SemaphoreOperator as _SemaphoreOperator,
-    BoundedSemaphoreOperator as _BoundedSemaphoreOperator,
-    EventOperator as _EventOperator,
-    ConditionOperator as _ConditionOperator,
-    LockAsyncOperator as _LockAsyncOperator,
-    SemaphoreAsyncOperator as _SemaphoreAsyncOperator,
-    BoundedSemaphoreAsyncOperator as _BoundedSemaphoreAsyncOperator,
-    EventAsyncOperator as _EventAsyncOperator,
-    ConditionAsyncOperator as _ConditionAsyncOperator,
-    QueueOperator as _QueueOperator)
+    LockOperator,
+    RLockOperator,
+    SemaphoreOperator,
+    BoundedSemaphoreOperator,
+    EventOperator,
+    ConditionOperator,
+    LockAsyncOperator,
+    SemaphoreAsyncOperator,
+    BoundedSemaphoreAsyncOperator,
+    EventAsyncOperator,
+    ConditionAsyncOperator,
+    QueueOperator)
 
-
-
-def LockOperator():
-    return __LockOperator()
-
-
-def RLockOperator():
-    return _RLockOperator()
-
-
-def SemaphoreOperator():
-    return _SemaphoreOperator()
-
-
-def BoundedSemaphoreOperator():
-    return _BoundedSemaphoreOperator()
-
-
-def EventOperator():
-    return _EventOperator()
-
-
-def ConditionOperator():
-    return _ConditionOperator()
-
-
-def LockAsyncOperator():
-    return _LockAsyncOperator()
-
-
-def SemaphoreAsyncOperator():
-    return _SemaphoreAsyncOperator()
-
-
-def BoundedSemaphoreAsyncOperator():
-    return _BoundedSemaphoreAsyncOperator()
-
-
-def EventAsyncOperator():
-    return _EventAsyncOperator()
-
-
-def ConditionAsyncOperator():
-    return _ConditionAsyncOperator()
-
-
-# def QueueOperator():
-#     return _QueueOperator
-
-QueueOperator = _QueueOperator
 
 RunWith = _RunWith
 AsyncRunWith = _AsyncRunWith
