793ba7ad84dfa65df4b49b1a667fefc91ad22090
Chisanan232 <chi10211201@cycu.org.tw>
2022-03-19 19:57:54 +0800

    Add some package info.

---
+++ multirunnable/__pkg_info__.py
@@ -0,0 +1,14 @@
+#    __     __)               _____                            
+#   (, /|  /|       /)     , (, /   )                 /)  /)   
+#     / | / |      // _/_      /__ /    __  __   _   (/_ //  _ 
+#  ) /  |/  |_(_(_(/_ (___(_) /   \_(_(_/ (_/ (_(_(_/_) (/__(/_
+# (_/   '                  (_/                                 
+
+__title__ = "MultiRunnable"
+__description__ = "A Python library integrates APIs of multiprocessing, threading, gevent and asyncio."
+__url__ = "https://multirunnable.readthedocs.io"
+__version__ = "0.17.0a1"
+__author__ = "Liu, Bryant"
+__author_email__ = "chi10211201@cycu.org.tw"
+__license__ = "Apache License 2.0"
+__copyright__ = 'Copyright 2022 Kenneth Reitz'
