57718aaba4e528b0252afd737e059041619999b5
Chisanan232 <chi10211201@cycu.org.tw>
2022-03-20 22:54:47 +0800

    Modify the implementations to follow the latest version of module '.persistence.database.layer'.

--- example/persistence/dao.py
+++ example/persistence/dao.py
@@ -4,20 +4,17 @@ from multirunnable.persistence.database.layer import BaseDao
 
 from db_mysql import MySQLSingleConnection, MySQLDriverConnectionPool, MySQLOperator
 
-import datetime
 import logging
 
 
 
 class TestingDao(BaseDao):
 
-    def __init__(self, db_driver=None, use_pool=False, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, db_driver=None, use_pool=False):
         self.db_driver = db_driver
         self.use_pool = use_pool
 
         # Initial and connect to database and get connection, cursor (or session) instance
-        self._database_opts = None
         self._database_config = {
             "host": "127.0.0.1",
             # "host": "172.17.0.6",
@@ -45,60 +42,25 @@ class TestingDao(BaseDao):
                 2. For many process, it still instantiate them so many times and gets so many different instances.
         """
 
+        super().__init__()
         self._logger = logging.getLogger(self.__class__.__name__)
 
 
-    @property
-    def database_opt(self) -> DatabaseOperator:
-        import multiprocessing as mp
-        print(f"TestDao:: Start to initial database operator instance. - {mp.current_process} - {datetime.datetime.now()}")
-
-        if self._database_opts is None:
-            print(f"TestDao:: It doesn't have the instance right now. - {mp.current_process} - {datetime.datetime.now()}")
-            if self.db_driver == "mysql":
-                # from db_mysql import MySQLSingleConnection, MySQLDriverConnectionPool, MySQLOperator
-                if self.use_pool is True:
-                    db_conn_strategy = MySQLDriverConnectionPool(**self._database_config)
-                else:
-                    db_conn_strategy = MySQLSingleConnection(**self._database_config)
-                print(f"TestDao:: It gets the strategy instance. strategy: {db_conn_strategy} - {mp.current_process}")
-                print(f"TestDao:: It gets the strategy instance. repr(strategy): {repr(db_conn_strategy)} - {mp.current_process}")
-                print(f"TestDao:: ID of strategy instance: {id(db_conn_strategy)} - {mp.current_process}")
-                self._database_opts = MySQLOperator(conn_strategy=db_conn_strategy)
-                print(f"TestDao:: It gets the operator instance. operator: {self._database_opts} - {mp.current_process}")
-                print(f"TestDao:: ID of operator instance: {id(self._database_opts)} - {mp.current_process}")
-
-            elif self.db_driver == "postgre":
-                from db_postgresql import PostgreSQLSingleConnection, PostgreSQLDriverConnectionPool, PostgreSQLOperator
-                if self.use_pool is True:
-                    db_conn_strategy = PostgreSQLDriverConnectionPool(**self._database_config)
-                else:
-                    db_conn_strategy = PostgreSQLSingleConnection(**self._database_config)
-                self._database_opts = PostgreSQLOperator(conn_strategy=db_conn_strategy)
-
-            elif self.db_driver == "cassandra":
-                from db_cassandra import CassandraSingleConnection, CassandraDriverConnectionPool, CassandraOperator
-                if self.use_pool is True:
-                    db_conn_strategy = CassandraDriverConnectionPool(**self._database_config)
-                else:
-                    db_conn_strategy = CassandraSingleConnection(**self._database_config)
-                self._database_opts = CassandraOperator(conn_strategy=db_conn_strategy)
-
-            elif self.db_driver == "clickhouse":
-                from db_clickhouse import ClickHouseSingleConnection, ClickHouseDriverConnectionPool, ClickHouseOperator
-                if self.use_pool is True:
-                    db_conn_strategy = ClickHouseDriverConnectionPool(**self._database_config)
-                else:
-                    db_conn_strategy = ClickHouseSingleConnection(**self._database_config)
-                self._database_opts = ClickHouseOperator(conn_strategy=db_conn_strategy)
-
+    def _instantiate_strategy(self) -> BaseDatabaseConnection:
+        if self.db_driver == "mysql":
+            # from db_mysql import MySQLSingleConnection, MySQLDriverConnectionPool, MySQLOperator
+            if self.use_pool is True:
+                db_conn_strategy = MySQLDriverConnectionPool(**self._database_config)
             else:
-                raise ValueError("")
+                db_conn_strategy = MySQLSingleConnection(**self._database_config)
+            return db_conn_strategy
         else:
-            print(f"TestDao:: Return instance directly because it already has it.")
+            raise ValueError
 
-        print(f"[DEBUG] ID of TestDao._database_opts: {id(self._database_opts)} - {mp.current_process()}")
-        return self._database_opts
+
+    def _instantiate_database_opts(self, strategy: BaseDatabaseConnection) -> DatabaseOperator:
+        _database_opts = MySQLOperator(conn_strategy=strategy)
+        return _database_opts
 
 
 
@@ -126,8 +88,8 @@ class TestDao(TestingDao):
         self._logger.debug(f"SQL tasks: {sql_tasks} - {mp.current_process()}")
         one_sql_query = sql_tasks.get()
         self._logger.debug(f"SQL tasks query: {one_sql_query} - {mp.current_process()}")
-        self.database_opt.execute(one_sql_query)
-        data = self.database_opt.fetch_all()
+        self.execute(one_sql_query)
+        data = self.fetch_all()
         return data
 
 
@@ -141,8 +103,8 @@ class TestDao(TestingDao):
         self._logger.debug(f"SQL tasks: {sql_tasks} - {mp.current_process()}")
         one_sql_query = sql_tasks.get()
         self._logger.debug(f"SQL tasks query: {one_sql_query} - {mp.current_process()}")
-        self.database_opt.execute(one_sql_query)
-        data = self.database_opt.fetch_all()
+        self.execute(one_sql_query)
+        data = self.fetch_all()
         return data
 
 
@@ -171,8 +133,8 @@ class AsyncTestDao(TestingDao):
         self._logger.debug(f"SQL tasks: {sql_tasks} - {mp.current_process()}")
         one_sql_query = await sql_tasks.get()
         self._logger.debug(f"SQL tasks query: {one_sql_query} - {mp.current_process()}")
-        self.database_opt.execute(one_sql_query)
-        data = self.database_opt.fetch_all()
+        self.execute(one_sql_query)
+        data = self.fetch_all()
         return data
 
 
@@ -186,6 +148,6 @@ class AsyncTestDao(TestingDao):
         self._logger.debug(f"SQL tasks: {sql_tasks} - {mp.current_process()}")
         one_sql_query = await sql_tasks.get()
         self._logger.debug(f"SQL tasks query: {one_sql_query} - {mp.current_process()}")
-        self.database_opt.execute(one_sql_query)
-        data = self.database_opt.fetch_all()
+        self.execute(one_sql_query)
+        data = self.fetch_all()
         return data
