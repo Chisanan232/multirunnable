2cf0440a26cb9ff766668ba0d542366735eab8c3
Chisanan232 <chi10211201@cycu.org.tw>
2022-04-03 16:00:44 +0800

    [Bug Fix] (config) Modify to use environment variables correctly.

--- scripts/database-execution.sh
+++ scripts/database-execution.sh
@@ -2,16 +2,16 @@
 
 if echo $1 | grep -q "show_databases";
 then
-  mysql -h ${{ env.DB_HOST }} -u ${{ env.DB_USER }} -p${{ env.DB_PASSWORD }} ${{ env.DB_DATABASE }} < ./tests/testing-database/test_sql/${{ env.DATABASE_DRIVER }}/show_database.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/show_database.sql
 elif echo $1 | grep -q "create_database";
 then
-  mysql -h ${{ env.DB_HOST }} -u ${{ env.DB_USER }} -p${{ env.DB_PASSWORD }} ${{ env.DB_DATABASE }} < ./tests/testing-database/test_sql/${{ env.DATABASE_DRIVER }}/create_database.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/create_database.sql
 elif echo $1 | grep -q "create_table";
 then
-  mysql -h ${{ env.DB_HOST }} -u ${{ env.DB_USER }} -p${{ env.DB_PASSWORD }} ${{ env.DB_DATABASE }} < ./tests/testing-database/test_sql/${{ env.DATABASE_DRIVER }}/create_table.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/create_table.sql
 elif echo $1 | grep -q "insert_test_data_rows";
 then
-  mysql -h ${{ env.DB_HOST }} -u ${{ env.DB_USER }} -p${{ env.DB_PASSWORD }} ${{ env.DB_DATABASE }} < ./tests/testing-database/test_sql/${{ env.DATABASE_DRIVER }}/insert_test_data_rows.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/insert_test_data_rows.sql
 else
   echo "error"
 fi
