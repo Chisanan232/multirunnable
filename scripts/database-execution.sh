8550f0bb2137984819b819ab6b6f3495ea3d23c0
Chisanan232 <chi10211201@cycu.org.tw>
2022-04-03 16:19:41 +0800

    [Bug Fix] (config) Remove option <database name> in shell.

--- scripts/database-execution.sh
+++ scripts/database-execution.sh
@@ -2,16 +2,16 @@
 
 if echo $1 | grep -q "show_databases";
 then
-  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/show_database.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD < ./tests/testing-database/test_sql/$DATABASE_DRIVER/show_database.sql
 elif echo $1 | grep -q "create_database";
 then
-  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/create_database.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD < ./tests/testing-database/test_sql/$DATABASE_DRIVER/create_database.sql
 elif echo $1 | grep -q "create_table";
 then
-  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/create_table.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD < ./tests/testing-database/test_sql/$DATABASE_DRIVER/create_table.sql
 elif echo $1 | grep -q "insert_test_data_rows";
 then
-  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE < ./tests/testing-database/test_sql/$DATABASE_DRIVER/insert_test_data_rows.sql
+  mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD < ./tests/testing-database/test_sql/$DATABASE_DRIVER/insert_test_data_rows.sql
 else
   echo "error"
 fi
