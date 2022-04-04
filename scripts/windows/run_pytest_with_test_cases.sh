9596f83d2a38da8af5bcaac68243b5807781203b
Chisanan232 <chi10211201@cycu.org.tw>
2022-04-05 00:21:30 +0800

    [Breaking Change] (config) Modify shell and commands. [skip actions]

--- scripts/windows/run_pytest_with_test_cases.sh
+++ scripts/windows/run_pytest_with_test_cases.sh
@@ -2,4 +2,7 @@
 
 current_file_content=$1
 handled_result=$(cat $current_file_content | sed "s/\"//g")
+echo $handled_result
+echo "Start to run pytest ..."
 pytest $handled_result
+echo "Run pytest done."
