103b8b13c32b183ac31bc586d8e94c0c9dbe6999
Chisanan232 <chi10211201@cycu.org.tw>
2022-04-06 06:59:55 +0800

    [New Feature] (script) Add new Python script for final test.

---
+++ scripts/mr_script.py
@@ -0,0 +1,36 @@
+from multirunnable import set_mode, RunningMode, SimpleExecutor
+from multirunnable.api import retry, RunWith
+from multirunnable.factory import BoundedSemaphoreFactory
+from multirunnable.adapter.context import context
+
+
+_Worker_Size = 7
+_Semaphore_Value = 2
+
+
+def target_function_with_bsmp() -> None:
+    retry_success_with_bsmp()
+
+
+@retry.function
+@RunWith.Bounded_Semaphore
+def retry_success_with_bsmp() -> str:
+    print("Running function ...")
+    _worker_name = context.get_current_worker_name()
+    return f"{_worker_name} Running Result"
+
+
+if __name__ == '__main__':
+
+    set_mode(RunningMode.Parallel)
+
+    _bsmp_factory = BoundedSemaphoreFactory(value=_Semaphore_Value)
+    _sexor = SimpleExecutor(executors=_Worker_Size)
+    _sexor.run(function=target_function_with_bsmp, features=_bsmp_factory)
+    _result = _sexor.result()
+
+    for _r in _result:
+        print("++++++++++++++++++++++++++++")
+        print(f"worker_name: {_r.worker_name}")
+        print(f"data: {_r.data}")
+        print(f"exception: {_r.exception}")
